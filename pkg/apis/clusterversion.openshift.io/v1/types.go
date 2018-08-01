35c854b8a75f7d115249c803b146e4883fe32124
Alex Crawford <crawford@redhat.com>
2018-08-01 14:07:54 -0700

    apis/cvo: introduce CVOConfig type
    This struct contains the runtime parameters to the CVO.

--- pkg/apis/clusterversion.openshift.io/v1/types.go
+++ pkg/apis/clusterversion.openshift.io/v1/types.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"github.com/google/uuid"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
@@ -74,6 +75,31 @@ const (
 	// encountered an error that is preventing it from working properly.
 	OperatorStatusConditionTypeDegraded OperatorStatusConditionType = "Degraded"
 )
+
+// CVOConfigList is a list of CVOConfig resources.
+// +k8s:deepcopy-gen=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type CVOConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []CVOConfig `json:"items"`
+}
+
+// CVOConfig is the configuration for the ClusterVersionOperator. This is where
+// parameters related to automatic updates can be set.
+// +genclient
+type CVOConfig struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Upstream  URL       `json:"upstream"`
+	Channel   string    `json:"channel"`
+	ClusterID uuid.UUID `json:"clusterId"`
+}
+
+// URL is a thin wrapper around string that ensures the string is a valid URL.
+type URL string
 
 // CVOStatus contains information specific to the ClusterVersionOperator. This
 // object is inserted into the Extension attribute of the generic
