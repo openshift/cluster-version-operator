4b485ca109cd271a10d32e47d3657331be8160fd
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2018-08-20 17:42:16 -0700

    pkg: update sync to fetchupdatepayload and apply payload
    * adds syncUpdatePayloadContents that ensures <prefix>/<version>
      contains the <version> update payload
    * adds syncUpdatePayload that uses lib.resourcebuilder to generically
      apply the update payload in order.

--- pkg/cvo/sync.go
+++ pkg/cvo/sync.go
@@ -2,83 +2,187 @@ package cvo
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
 	"time"
 
 	"github.com/golang/glog"
+	"github.com/openshift/cluster-version-operator/lib"
 	"github.com/openshift/cluster-version-operator/lib/resourceapply"
+	"github.com/openshift/cluster-version-operator/lib/resourcebuilder"
 	"github.com/openshift/cluster-version-operator/pkg/apis"
 	"github.com/openshift/cluster-version-operator/pkg/apis/clusterversion.openshift.io/v1"
-	"github.com/openshift/cluster-version-operator/pkg/version"
 	appsv1 "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	apiextv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	randutil "k8s.io/apimachinery/pkg/util/rand"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/utils/pointer"
 )
 
-func (optr *Operator) syncCVODeploy(config *v1.CVOConfig) error {
-	if config.DesiredUpdate.Payload == "" || config.DesiredUpdate.Version == "" || config.DesiredUpdate.Version == version.Version.String() {
-		glog.V(4).Info("no update to be applied.")
-		return nil
+func (optr *Operator) syncUpdatePayload(config *v1.CVOConfig, payload [][]lib.Manifest) error {
+	for _, manifests := range payload {
+		for _, manifest := range manifests {
+			glog.V(4).Infof("Running sync for (%s) %s/%s", manifest.GVK.String(), manifest.Object().GetNamespace(), manifest.Object().GetName())
+			b, err := resourcebuilder.New(optr.restConfig, manifest)
+			if err != nil {
+				return err
+			}
+			if err := b.Do(ownerRefModifier(config)); err != nil {
+				return err
+			}
+			glog.V(4).Infof("Done syncing for (%s) %s/%s", manifest.GVK.String(), manifest.Object().GetNamespace(), manifest.Object().GetName())
+		}
 	}
-	cvo := &appsv1.Deployment{
+	return nil
+}
+
+func ownerRefModifier(config *v1.CVOConfig) resourcebuilder.MetaV1ObjectModifierFunc {
+	oref := metav1.NewControllerRef(config, ownerKind)
+	return func(obj metav1.Object) {
+		obj.SetOwnerReferences([]metav1.OwnerReference{*oref})
+	}
+}
+
+const (
+	updatePayloadsPathPrefix = "/etc/cvo/update-payloads"
+)
+
+func (optr *Operator) syncUpdatePayloadContents(pathprefix string, config *v1.CVOConfig) ([][]lib.Manifest, error) {
+	if !updatedesired(config.DesiredUpdate) {
+		return nil, nil
+	}
+	dv := config.DesiredUpdate.Version
+	dp := config.DesiredUpdate.Payload
+
+	dirpath := filepath.Join(pathprefix, dv)
+	_, err := os.Stat(dirpath)
+	if err != nil && !os.IsNotExist(err) {
+		return nil, err
+	}
+	if os.IsNotExist(err) {
+		if err := optr.fetchUpdatePayloadToPath(pathprefix, config); err != nil {
+			return nil, err
+		}
+	}
+
+	// read dirpath to manifests:
+	// For each operator in payload the manifests should be ordered as:
+	// 1. CRDs
+	// 2. others
+	mmap, err := lib.LoadManifests(dirpath)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(mmap) == 0 {
+		return nil, fmt.Errorf("empty update payload %s", dp)
+	}
+
+	sortedkeys := []string{}
+	for k := range mmap {
+		sortedkeys = append(sortedkeys, k)
+	}
+	sort.Strings(sortedkeys)
+
+	payload := [][]lib.Manifest{}
+	for _, k := range sortedkeys {
+		ordered := orderManifests(mmap[k])
+		payload = append(payload, ordered)
+	}
+	return payload, nil
+}
+
+func (optr *Operator) fetchUpdatePayloadToPath(pathprefix string, config *v1.CVOConfig) error {
+	if !updatedesired(config.DesiredUpdate) {
+		return fmt.Errorf("error DesiredUpdate is empty")
+	}
+	var (
+		version         = config.DesiredUpdate.Version
+		payload         = config.DesiredUpdate.Payload
+		name            = fmt.Sprintf("%s-%s-%s", optr.name, version, randutil.String(5))
+		namespace       = optr.namespace
+		deadline        = pointer.Int64Ptr(2 * 60)
+		nodeSelectorKey = "node-role.kubernetes.io/master"
+		nodename        = optr.nodename
+		vpath           = filepath.Join(pathprefix, version)
+		cmd             = []string{"/bin/sh"}
+		args            = []string{"-c", fmt.Sprintf("mv /manifests %s", vpath)}
+	)
+
+	job := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      optr.name,
-			Namespace: optr.namespace,
-			Labels: map[string]string{
-				"k8s-app": optr.name,
-			},
+			Name:      name,
+			Namespace: namespace,
 		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: pointer.Int32Ptr(1),
-			Strategy: appsv1.DeploymentStrategy{
-				Type: appsv1.RollingUpdateDeploymentStrategyType,
-				RollingUpdate: &appsv1.RollingUpdateDeployment{
-					MaxUnavailable: intOrStringPtr(intstr.FromInt(0)),
-					MaxSurge:       intOrStringPtr(intstr.FromInt(1)),
-				},
-			},
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"k8s-app": optr.name,
-				},
-			},
+		Spec: batchv1.JobSpec{
+			ActiveDeadlineSeconds: deadline,
 			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"k8s-app": optr.name,
-					},
-				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{{
-						Name:  optr.name,
-						Image: config.DesiredUpdate.Payload,
+						Name:    "payload",
+						Image:   payload,
+						Command: cmd,
+						Args:    args,
+						VolumeMounts: []corev1.VolumeMount{{
+							MountPath: pathprefix,
+							Name:      "payload",
+						}},
 					}},
+					Volumes: []corev1.Volume{{
+						Name: "payload",
+						VolumeSource: corev1.VolumeSource{
+							HostPath: &corev1.HostPathVolumeSource{
+								Path: pathprefix,
+							},
+						},
+					}},
+					NodeName: nodename,
 					NodeSelector: map[string]string{
-						"node-role.kubernetes.io/master": "",
+						nodeSelectorKey: "",
 					},
 					Tolerations: []corev1.Toleration{{
-						Key: "node-role.kubernetes.io/master",
+						Key: nodeSelectorKey,
 					}},
+					RestartPolicy: corev1.RestartPolicyOnFailure,
 				},
 			},
 		},
 	}
 
-	glog.V(4).Infof("Updating CVO to %s...", config.DesiredUpdate.Version)
-	_, updated, err := resourceapply.ApplyDeploymentFromCache(optr.deployLister, optr.kubeClient.AppsV1(), cvo)
+	_, err := optr.kubeClient.BatchV1().Jobs(job.Namespace).Create(job)
 	if err != nil {
 		return err
 	}
-	if updated {
-		if err := optr.waitForDeploymentRollout(cvo); err != nil {
-			return err
+	return resourcebuilder.WaitForJobCompletion(optr.kubeClient.BatchV1(), job)
+}
+
+func orderManifests(manifests []lib.Manifest) []lib.Manifest {
+	var crds, others []lib.Manifest
+	for _, m := range manifests {
+		group, version, kind := m.GVK.Group, m.GVK.Version, m.GVK.Kind
+		switch {
+		case group == apiextv1beta1.SchemeGroupVersion.Group && version == apiextv1beta1.SchemeGroupVersion.Version && kind == resourcebuilder.CRDKind:
+			crds = append(crds, m)
+		default:
+			others = append(others, m)
 		}
 	}
-	return nil
+
+	out := []lib.Manifest{}
+	out = append(out, crds...)
+	out = append(out, others...)
+	return out
+}
+
+func updatedesired(desired v1.Update) bool {
+	return desired.Payload != "" &&
+		desired.Version != ""
 }
 
 func (optr *Operator) syncCVOCRDs() error {
