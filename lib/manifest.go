3e203bec1bc66dc1614fce51e6ed071ba33b7c2d
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2018-09-18 19:03:57 -0700

    lib: update manifest to read from files

--- lib/manifest.go
+++ lib/manifest.go
@@ -5,10 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
-	"path/filepath"
-	"sort"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -61,108 +58,22 @@ func (m *Manifest) UnmarshalJSON(in []byte) error {
 // Object returns underlying metav1.Object
 func (m *Manifest) Object() metav1.Object { return m.obj }
 
-const (
-	// rootDirKey is used as key for the manifest files in root dir
-	// passed to LoadManifests
-	// It is set to `000` to give it more priority if the actor sorts
-	// based on keys.
-	rootDirKey = "000"
-)
-
-// LoadManifests loads manifest from disk.
-//
-// root/
-//     manifest0
-//     manifest1
-//     00_subdir0/
-//         manifest0
-//         manifest1
-//     01_subdir1/
-//         manifest0
-//         manifest1
-// LoadManifests(<abs path to>/root):
-// returns map
-// 000: [manifest0, manifest1]
-// 00_subdir0: [manifest0, manifest1]
-// 01_subdir1: [manifest0, manifest1]
-//
-// It skips dirs that have not files.
-// It only reads dir `p` and its direct subdirs.
-func LoadManifests(p string) (map[string][]Manifest, error) {
-	var out = make(map[string][]Manifest)
-
-	fs, err := ioutil.ReadDir(p)
-	if err != nil {
-		return nil, err
-	}
-
-	// We want to accumulate all the errors, not returning at the
-	// first error encountered when reading subdirs.
-	var errs []error
-
-	// load manifest files in p to rootDirKey
-	ms, err := loadManifestsFromDir(p)
-	if err != nil {
-		errs = append(errs, fmt.Errorf("error loading from dir %s: %v", p, err))
-	}
-	if len(ms) > 0 {
-		out[rootDirKey] = ms
-	}
-
-	// load manifests from subdirs to subdir-name
-	for _, f := range fs {
-		if !f.IsDir() {
-			continue
-		}
-		path := filepath.Join(p, f.Name())
-		ms, err := loadManifestsFromDir(path)
-		if err != nil {
-			errs = append(errs, fmt.Errorf("error loading from dir %s: %v", path, err))
-			continue
-		}
-		if len(ms) > 0 {
-			out[f.Name()] = ms
-		}
-	}
-
-	agg := utilerrors.NewAggregate(errs)
-	if agg != nil {
-		return nil, errors.New(agg.Error())
-	}
-	return out, nil
-}
-
-// loadManifestsFromDir only returns files. not subdirs are traversed.
-// returns manifests in increasing order of their filename.
-func loadManifestsFromDir(dir string) ([]Manifest, error) {
+// ManifestsFromFiles reads files and returns Manifests in the same order.
+// files should be list of absolute paths for the manifests on disk.
+func ManifestsFromFiles(files []string) ([]Manifest, error) {
 	var manifests []Manifest
-	fs, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return nil, err
-	}
-
-	// ensure sorted.
-	sort.Slice(fs, func(i, j int) bool {
-		return fs[i].Name() < fs[j].Name()
-	})
-
 	var errs []error
-	for _, f := range fs {
-		if f.IsDir() {
-			continue
-		}
-
-		path := filepath.Join(dir, f.Name())
-		file, err := os.Open(path)
+	for _, file := range files {
+		file, err := os.Open(file)
 		if err != nil {
-			errs = append(errs, fmt.Errorf("error opening %s: %v", path, err))
+			errs = append(errs, fmt.Errorf("error opening %s: %v", file.Name(), err))
 			continue
 		}
 		defer file.Close()
 
 		ms, err := parseManifests(file)
 		if err != nil {
-			errs = append(errs, fmt.Errorf("error parsing %s: %v", path, err))
+			errs = append(errs, fmt.Errorf("error parsing %s: %v", file.Name(), err))
 			continue
 		}
 		manifests = append(manifests, ms...)
@@ -170,7 +81,7 @@ func loadManifestsFromDir(dir string) ([]Manifest, error) {
 
 	agg := utilerrors.NewAggregate(errs)
 	if agg != nil {
-		return nil, fmt.Errorf("error loading manifests from %q: %v", dir, agg.Error())
+		return nil, fmt.Errorf("error loading manifests: %v", agg.Error())
 	}
 
 	return manifests, nil
