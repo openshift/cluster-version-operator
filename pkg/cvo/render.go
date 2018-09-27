904950728c0b96118b479a12b72a9e806cf14e89
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2018-09-27 07:34:24 -0700

    pkg/cvo: render `/manifests` rather the entire updatepayload
    CVO render only needs to render the `/manifests`. All other manifests
    will be applied when CVO is running.

--- pkg/cvo/render.go
+++ pkg/cvo/render.go
@@ -4,55 +4,92 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
-	"strings"
 	"text/template"
 
-	"github.com/golang/glog"
-	batchv1 "k8s.io/api/batch/v1"
-	batchv1beta1 "k8s.io/api/batch/v1beta1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
-
-	osv1 "github.com/openshift/cluster-version-operator/pkg/apis/operatorstatus.openshift.io/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
-// Render renders all the manifests from updatepayload to outputDir.
-// Render skips Jobs, OperatorStatus.
+// Render renders all the manifests from /manifests to outputDir.
 func Render(outputDir, releaseImage string) error {
-	up, err := loadUpdatePayload(defaultUpdatePayloadDir, releaseImage)
-	if err != nil {
-		return fmt.Errorf("error loading update payload from %q: %v", defaultUpdatePayloadDir, err)
-	}
+	var (
+		manifestsDir  = "/manifests"
+		oManifestsDir = filepath.Join(outputDir, "manifests")
+		bootstrapDir  = "/bootstrap"
+		oBootstrapDir = filepath.Join(outputDir, "bootstrap")
 
+		renderConfig = manifestRenderConfig{ReleaseImage: releaseImage}
+	)
+
+	tasks := []struct {
+		idir      string
+		odir      string
+		skipFiles sets.String
+	}{{
+		idir:      manifestsDir,
+		odir:      oManifestsDir,
+		skipFiles: sets.NewString("image-references"),
+	}, {
+		idir:      bootstrapDir,
+		odir:      oBootstrapDir,
+		skipFiles: sets.NewString(),
+	}}
 	var errs []error
-	skipGVKs := []schema.GroupVersionKind{
-		batchv1.SchemeGroupVersion.WithKind("Job"), batchv1beta1.SchemeGroupVersion.WithKind("Job"),
-		osv1.SchemeGroupVersion.WithKind("OperatorStatus"),
-	}
-	for idx, manifest := range up.manifests {
-		mname := fmt.Sprintf("(%s) %s/%s", manifest.GVK, manifest.Object().GetNamespace(), manifest.Object().GetName())
-		skip := false
-		for _, gvk := range skipGVKs {
-			if gvk == manifest.GVK {
-				skip = true
-			}
-		}
-		if skip {
-			glog.Infof("skipping Manifest %s", mname)
-			continue
-		}
-
-		filename := strings.ToLower(fmt.Sprintf("%03d-%s-%s-%s-%s-%s", idx, manifest.GVK.Group, manifest.GVK.Version, manifest.GVK.Kind, manifest.Object().GetNamespace(), manifest.Object().GetName()))
-		path := filepath.Join(outputDir, filename)
-		if err := ioutil.WriteFile(path, manifest.Raw, 0644); err != nil {
+	for _, task := range tasks {
+		if err := renderDir(renderConfig, task.idir, task.odir, task.skipFiles); err != nil {
 			errs = append(errs, err)
 		}
 	}
 
 	agg := utilerrors.NewAggregate(errs)
 	if agg != nil {
-		return fmt.Errorf("error rendering from UpdatePayload: %v", agg.Error())
+		return fmt.Errorf("error rendering manifests: %v", agg.Error())
+	}
+	return nil
+}
+
+func renderDir(renderConfig manifestRenderConfig, idir, odir string, skipFiles sets.String) error {
+	if err := os.MkdirAll(odir, 0666); err != nil {
+		return err
+	}
+	files, err := ioutil.ReadDir(idir)
+	if err != nil {
+		return err
+	}
+	var errs []error
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		if skipFiles.Has(file.Name()) {
+			continue
+		}
+
+		ipath := filepath.Join(idir, file.Name())
+		iraw, err := ioutil.ReadFile(ipath)
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		rraw, err := renderManifest(renderConfig, iraw)
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		opath := filepath.Join(odir, file.Name())
+		if err := ioutil.WriteFile(opath, rraw, 0666); err != nil {
+			errs = append(errs, err)
+			continue
+		}
+	}
+
+	agg := utilerrors.NewAggregate(errs)
+	if agg != nil {
+		return fmt.Errorf("error rendering manifests: %v", agg.Error())
 	}
 	return nil
 }
