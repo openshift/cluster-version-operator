af365dfd643408d28a1f6fdddc7c257071c85b70
Abhinav Dahiya <abhinav.dahiya@redhat.com>
2019-08-07 17:16:23 -0700

    pkg/cvo/metrics/go: the cluster operators report Degraded and not Failing
    Cherry-picked from fad0688c34 (pkg/cvo/metrics/go: the cluster
    operators report Degraded and not Failing, 2019-08-07, #232) and
    manually removed the import shuffling from metrics.go.

--- pkg/cvo/metrics.go
+++ pkg/cvo/metrics.go
@@ -263,7 +263,7 @@ func (m *operatorMetrics) Collect(ch chan<- prometheus.Metric) {
 			break
 		}
 		g := m.clusterOperatorUp.WithLabelValues(op.Name, firstVersion)
-		failing := resourcemerge.IsOperatorStatusConditionTrue(op.Status.Conditions, ClusterStatusFailing)
+		failing := resourcemerge.IsOperatorStatusConditionTrue(op.Status.Conditions, configv1.OperatorDegraded)
 		available := resourcemerge.IsOperatorStatusConditionTrue(op.Status.Conditions, configv1.OperatorAvailable)
 		if available && !failing {
 			g.Set(1)
