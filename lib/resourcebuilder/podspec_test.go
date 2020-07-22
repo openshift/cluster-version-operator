f0f00ff9b9b184a306676050feb8d5bc6e8f0443
David Eads <deads@redhat.com>
2020-07-22 10:08:37 -0400

    update CVO to inject internal loadbalancer for use by the CVO pod
    During upgrades and kube-apiserver rollouts, the localhost connection can become
    unavailable for multiple minutes. Using the internal loadbalancer prevents the
    outage and lease loss.

--- lib/resourcebuilder/podspec_test.go
+++ lib/resourcebuilder/podspec_test.go
@@ -110,3 +110,159 @@ func TestUpdatePodSpecWithProxy(t *testing.T) {
 		})
 	}
 }
+
+func TestUpdatePodSpecWithInternalLoadBalancerKubeService(t *testing.T) {
+	tests := []struct {
+		name string
+
+		input          *corev1.PodSpec
+		containerNames []string
+		lbHost, lbPort string
+
+		expectedErr string
+		expected    *corev1.PodSpec
+	}{
+		{
+			name: "no lbhost val",
+			input: &corev1.PodSpec{
+				Containers: []corev1.Container{
+					{
+						Name: "foo",
+					},
+				},
+			},
+			expected: &corev1.PodSpec{
+				Containers: []corev1.Container{
+					{
+						Name: "foo",
+					},
+				},
+			},
+		},
+		{
+			name:           "host and port, add to container and mutate",
+			containerNames: []string{"foo", "init-foo"},
+			lbHost:         "lbhost-val",
+			lbPort:         "lbport-val",
+			input: &corev1.PodSpec{
+				InitContainers: []corev1.Container{
+					{
+						Name: "init-foo",
+						Env: []corev1.EnvVar{
+							{Name: "KUBERNETES_SERVICE_PORT", Value: "oldport-val"},
+							{Name: "KUBERNETES_SERVICE_HOST", Value: "oldhost-val"},
+						},
+					},
+					{
+						Name: "init-bar",
+					},
+				},
+				Containers: []corev1.Container{
+					{
+						Name: "foo",
+					},
+					{
+						Name: "bar",
+					},
+				},
+			},
+			expected: &corev1.PodSpec{
+				InitContainers: []corev1.Container{
+					{
+						Name: "init-foo",
+						Env: []corev1.EnvVar{
+							{Name: "KUBERNETES_SERVICE_PORT", Value: "lbport-val"},
+							{Name: "KUBERNETES_SERVICE_HOST", Value: "lbhost-val"},
+						},
+					},
+					{
+						Name: "init-bar",
+					},
+				},
+				Containers: []corev1.Container{
+					{
+						Name: "foo",
+						Env: []corev1.EnvVar{
+							{Name: "KUBERNETES_SERVICE_PORT", Value: "lbport-val"},
+							{Name: "KUBERNETES_SERVICE_HOST", Value: "lbhost-val"},
+						},
+					},
+					{
+						Name: "bar",
+					},
+				},
+			},
+		},
+		{
+			name:           "lbhost only, add to container and mutate",
+			containerNames: []string{"foo", "init-foo"},
+			lbHost:         "lbhost-val",
+			input: &corev1.PodSpec{
+				InitContainers: []corev1.Container{
+					{
+						Name: "init-foo",
+						Env: []corev1.EnvVar{
+							{Name: "KUBERNETES_SERVICE_PORT", Value: "oldport-val"},
+							{Name: "KUBERNETES_SERVICE_HOST", Value: "oldhost-val"},
+						}},
+					{
+						Name: "init-bar",
+					},
+				},
+				Containers: []corev1.Container{
+					{
+						Name: "foo",
+					},
+					{
+						Name: "bar",
+					},
+				},
+			},
+			expected: &corev1.PodSpec{
+				InitContainers: []corev1.Container{
+					{
+						Name: "init-foo",
+						Env: []corev1.EnvVar{
+							{Name: "KUBERNETES_SERVICE_PORT", Value: "443"},
+							{Name: "KUBERNETES_SERVICE_HOST", Value: "lbhost-val"},
+						},
+					},
+					{
+						Name: "init-bar",
+					},
+				},
+				Containers: []corev1.Container{
+					{
+						Name: "foo",
+						Env: []corev1.EnvVar{
+							{Name: "KUBERNETES_SERVICE_PORT", Value: "443"},
+							{Name: "KUBERNETES_SERVICE_HOST", Value: "lbhost-val"},
+						},
+					},
+					{
+						Name: "bar",
+					},
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := updatePodSpecWithInternalLoadBalancerKubeService(test.input, test.containerNames, test.lbHost, test.lbPort)
+			switch {
+			case err == nil && len(test.expectedErr) == 0:
+			case err != nil && len(test.expectedErr) == 0:
+				t.Fatal(err)
+			case err == nil && len(test.expectedErr) != 0:
+				t.Fatal(err)
+			case err != nil && len(test.expectedErr) != 0 && err.Error() != test.expectedErr:
+				t.Fatal(err)
+			}
+
+			if !reflect.DeepEqual(test.input, test.expected) {
+				t.Error(diff.ObjectDiff(test.input, test.expected))
+			}
+		})
+	}
+}
