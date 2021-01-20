c3dc8b64363e2c690471d680dce93c078bde0153
Lalatendu Mohanty <lmohanty@redhat.com>
2021-01-20 17:51:18 +0530

    Bug 1918085: Fixing the log message in WaitForJobCompletion()
    Signed-off-by: Lalatendu Mohanty <lmohanty@redhat.com>

--- lib/resourcebuilder/batch.go
+++ lib/resourcebuilder/batch.go
@@ -18,7 +18,7 @@ func WaitForJobCompletion(ctx context.Context, client batchclientv1.JobsGetter, 
 			klog.Error(err)
 			return false, nil
 		} else if !done {
-			klog.V(4).Infof("Job %s in namespace %s is not ready, continuing to wait.", job.ObjectMeta.Namespace, job.ObjectMeta.Name)
+			klog.V(4).Infof("Job %s in namespace %s is not ready, continuing to wait.", job.ObjectMeta.Name, job.ObjectMeta.Namespace)
 			return false, nil
 		}
 		return true, nil
