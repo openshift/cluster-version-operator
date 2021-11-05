fa6ff3709823e907f0eeb6c9f135ee994c184f55
W. Trevor King <wking@tremily.us>
2021-11-04 23:24:09 -0700

    pkg/clusterconditions/promql: Fill in the implementation
    The Thanos Service requests a TLS certificate, as documented in [1]:
    
      $ curl -s https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/origin-ci-test/logs/periodic-ci-openshift-release-master-ci-4.10-e2e-gcp/1457166426941952000/artifacts/e2e-gcp/gather-extra/artifacts/services.json | jq -r '.items[].metadata | select(.name == "thanos-querier").annotations'
      {
        "service.alpha.openshift.io/serving-cert-signed-by": "openshift-service-serving-signer@1636251390",
        "service.beta.openshift.io/serving-cert-secret-name": "thanos-querier-tls",
        "service.beta.openshift.io/serving-cert-signed-by": "openshift-service-serving-signer@1636251390"
      }
    
    The service-ca controller [2] provides a Secret with the requested
    cert and key:
    
      $ curl -s https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com/gcs/origin-ci-test/logs/periodic-ci-openshift-release-master-ci-4.10-e2e-gcp/1457166426941952000/artifacts/e2e-gcp/gather-must-gather/artifacts/must-gather.tar | tar xOz registry-ci-openshift-org-ocp-4-10-2021-11-07-014119-sha256-bec152bc664c5ba6192357aabfcdd18810135c8e89800239ffee86b6a5d8730d/namespaces/openshift-monitoring/core/secrets.yaml | yaml2json | jq -r '.items[] | select(.metadata.name == "thanos-querier-tls").data["tls.crt"] | @base64d' | openssl x509 -text -noout | grep 'Issuer\|Subject:\|DNS'
              Issuer: CN = openshift-service-serving-signer@1636251390
              Subject: CN = thanos-querier.openshift-monitoring.svc
                      DNS:thanos-querier.openshift-monitoring.svc, DNS:thanos-querier.openshift-monitoring.svc.cluster.local
    
    From [3]:
    
      Other pods can trust cluster-created certificates (which are only
      signed for internal DNS names), by using the CA bundle in the
      /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt file
      that is automatically mounted in their pod.
    
    However, checking a CVO term in a 4.10 cluster:
    
      sh-4.4# ls /var/run/secrets/kubernetes.io/serviceaccount
      ca.crt  namespace  token
    
    So instead I'm mounting the openshift-service-ca.crt ConfigMap, which
    has the service.beta.openshift.io/inject-cabundle annotation.  I'm not
    entirely sure what the flow is for creating this ConfigMap.  It's
    metadata.managedFields[].manager is kube-controller-manager; maybe
    similar ConfigMaps are created in all namespaces?
    
    [1]: https://docs.openshift.com/container-platform/4.9/security/certificate_types_descriptions/service-ca-certificates.html
    [2]: https://docs.openshift.com/container-platform/4.9/security/certificates/service-serving-certificate.html
    [3]: https://docs.openshift.com/container-platform/4.9/nodes/pods/nodes-pods-secrets.html#nodes-pods-secrets-certificates-about_nodes-pods-secrets

--- pkg/clusterconditions/promql/promql.go
+++ pkg/clusterconditions/promql/promql.go
@@ -1,4 +1,4 @@
-// Package promql
+// Package promql implements a cluster condition based on PromQL queries.
 //
 // https://github.com/openshift/enhancements/blob/master/enhancements/update/targeted-update-edge-blocking.md#promql
 package promql
@@ -6,15 +6,40 @@ package promql
 import (
 	"context"
 	"errors"
+	"fmt"
+	"time"
 
 	configv1 "github.com/openshift/api/config/v1"
+	"github.com/prometheus/client_golang/api"
+	prometheusv1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/config"
+	"github.com/prometheus/common/model"
+	"k8s.io/klog/v2"
+
 	"github.com/openshift/cluster-version-operator/pkg/clusterconditions"
 )
 
 // PromQL implements a cluster condition that matches based on PromQL.
-type PromQL struct{}
+type PromQL struct {
+	// Address holds the Prometheus query URI.
+	Address string
 
-var promql = &PromQL{}
+	// HTTPClientConfig holds the client configuration for connecting to the Prometheus service.
+	HTTPClientConfig config.HTTPClientConfig
+}
+
+var promql = &PromQL{
+	Address: "https://thanos-querier.openshift-monitoring.svc.cluster.local:9091",
+	HTTPClientConfig: config.HTTPClientConfig{
+		Authorization: &config.Authorization{
+			Type:            "Bearer",
+			CredentialsFile: "/var/run/secrets/kubernetes.io/serviceaccount/token",
+		},
+		TLSConfig: config.TLSConfig{
+			CAFile: "/etc/tls/service-ca/service-ca.crt",
+		},
+	},
+}
 
 // Valid returns an error if the condition contains any properties
 // besides 'type' and a valid `promql`.
@@ -34,7 +59,50 @@ func (p *PromQL) Valid(ctx context.Context, condition *configv1.ClusterCondition
 // false when the PromQL evaluates to 0, and an error if the PromQL
 // returns no time series or returns a value besides 0 or 1.
 func (p *PromQL) Match(ctx context.Context, condition *configv1.ClusterCondition) (bool, error) {
-	return false, errors.New("not yet implemented: PromQL matching")
+	clientConfig := api.Config{Address: p.Address}
+
+	if roundTripper, err := config.NewRoundTripperFromConfig(p.HTTPClientConfig, "cluster-conditions"); err == nil {
+		clientConfig.RoundTripper = roundTripper
+	} else {
+		return false, fmt.Errorf("creating PromQL round-tripper: %w", err)
+	}
+
+	client, err := api.NewClient(clientConfig)
+	if err != nil {
+		return false, fmt.Errorf("creating PromQL client: %w", err)
+	}
+
+	v1api := prometheusv1.NewAPI(client)
+	klog.V(4).Infof("evaluate %s cluster condition: %q", condition.Type, condition.PromQL.PromQL)
+	result, warnings, err := v1api.Query(ctx, condition.PromQL.PromQL, time.Now())
+	if err != nil {
+		return false, fmt.Errorf("executing PromQL query: %w", err)
+	}
+
+	for _, warning := range warnings {
+		klog.Warning(warning)
+	}
+
+	if result.Type() != model.ValVector {
+		return false, fmt.Errorf("invalid PromQL result type is %s, not vector", result.Type())
+	}
+
+	vector, ok := result.(model.Vector)
+	if !ok {
+		return false, fmt.Errorf("invalid PromQL result type is nominally %s, but fails Vector cast", result.Type())
+	}
+
+	if vector.Len() != 1 {
+		return false, fmt.Errorf("invalid PromQL result length must be one, but is %d", vector.Len())
+	}
+
+	sample := vector[0]
+	if sample.Value == 0 {
+		return false, nil
+	} else if sample.Value == 1 {
+		return true, nil
+	}
+	return false, fmt.Errorf("invalid PromQL result (must be 0 or 1): %v", sample.Value)
 }
 
 func init() {
