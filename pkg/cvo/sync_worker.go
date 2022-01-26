8b954902a61f18f866464bf235663240112d3d1c
W. Trevor King <wking@tremily.us>
2022-01-25 16:02:50 -0800

    pkg/cvo/sync_worker: Log transition to updating
    To make it easier to understand why this is happening.

--- pkg/cvo/sync_worker.go
+++ pkg/cvo/sync_worker.go
@@ -439,6 +439,7 @@ func (w *SyncWorker) calculateNext(work *SyncWork) bool {
 		work.State = w.work.State
 		work.Attempt = 0
 	} else if changed && w.work.State != payload.InitializingPayload {
+		klog.V(2).Infof("Work changed, transitioning from %s and the suggested incoming %s state to %s", work.State, w.work.State, payload.UpdatingPayload)
 		work.State = payload.UpdatingPayload
 		work.Attempt = 0
 	}
