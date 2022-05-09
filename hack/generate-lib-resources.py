openshift/cluster-version-operator

commit e8ffccb87821241adfb5c2da9b5053a4a53705a3

lib: Add autogeneration for some resource* functionality
Protecting us from [1,2]:

  converting (v1beta1.Role) to (v1.Role): unknown conversion

Because merging and application are also version-dependent, it's hard
to paper over this in resourceread with automatic type conversion.
Although from [3]:

  Promotes the rbac.authorization.k8s.io/v1beta1 API to v1 with no changes

so all we really need is the apiVersion bump.  Anyhow, with this
commit, I'm doubling down on the approach from 4ee7b07e99 (Add
apiextensions.k8s.io/v1 support for CRDs, 2019-10-22, #259) and
collapsing the readers into two helpers that support all of our types
and return runtime.Object.

From 0a255aba5c (cvo: Use protobuf for sending events and other basic
API commands, 2019-01-18, #90), protobuf is more efficient, so we
should use it where possible.

And because all of this is very tedious to maintain by hand, there's
now a Python generator to spit out all the boilerplate.

[1]: https://github.com/kubernetes/kubernetes/pull/90018
[2]: https://github.com/openshift/cluster-version-operator/pull/420#issuecomment-665960650
[3]: https://github.com/kubernetes/kubernetes/pull/49642

hack/generate-lib-resources.py
@@ -1,0 +1,300 @@
+#!/usr/bin/env python
+
+import os.path
+
+
+def generate_lib_resources(directory, types, clients, modifiers, health_checks):
+    generate_resourceread(directory=os.path.join(directory, 'resourceread'), types=types)
+    generate_resourcebuilder(directory=os.path.join(directory, 'resourcebuilder'), types=types, clients=clients, modifiers=modifiers, health_checks=health_checks)
+
+
+def generate_resourceread(directory, types):
+    package_name = os.path.basename(directory)
+    path = os.path.join(directory, package_name + '.go')
+    lines = [
+        '// auto-generated with generate-lib-resources.py',
+        '',
+        '// Package {} reads supported objects from bytes.'.format(package_name),
+        'package {}'.format(package_name),
+        '',
+        'import (',
+    ]
+
+    imports = {}
+    for import_name in [
+            'k8s.io/apimachinery/pkg/runtime',
+            'k8s.io/apimachinery/pkg/runtime/serializer',
+            ]:
+        imports[import_name] = '\t"{}"'.format(import_name)
+
+    for package in sorted(types.keys()):
+        base, version = os.path.split(package)  # FIXME: should be using network path operations on package names
+        short_name = os.path.basename(base)
+        imports[package] = '\t{}{} "{}"'.format(short_name, version, package)
+
+    lines.extend([import_line for _, import_line in sorted(imports.items(), key=lambda package_line: package_line[0])])
+    lines.extend([
+        ')',
+        '',
+        'var (',
+        '\tscheme  = runtime.NewScheme()',
+        '\tcodecs  = serializer.NewCodecFactory(scheme)',
+        '\tdecoder runtime.Decoder',
+        ')',
+        '',
+        'func init() {',
+    ])
+
+    sgvs = scheme_group_versions(types=types)
+    for _, data in sorted(sgvs.items()):
+        lines.extend([
+            '\tif err := {0}{1}.AddToScheme(scheme); err != nil {{'.format(data['short_name'], data['version']),
+            '\t\tpanic(err)',
+            '\t}',
+        ])
+
+    lines.append('\tdecoder = codecs.UniversalDecoder(')
+    lines.extend(['\t\t{},'.format(sgv) for sgv in sorted(sgvs.keys())])
+    lines.extend([
+        '\t)',
+        '}',
+        '',
+        '// Read reads an object from bytes.',
+        'func Read(objBytes []byte) (runtime.Object, error) {',
+        '\treturn runtime.Decode(decoder, objBytes)',
+        '}',
+        '',
+        '// ReadOrDie reads an object from bytes.  Panics on error.',
+        'func ReadOrDie(objBytes []byte) runtime.Object {',
+        '\trequiredObj, err := runtime.Decode(decoder, objBytes)',
+        '\tif err != nil {',
+        '\t\tpanic(err)',
+        '\t}',
+        '\treturn requiredObj',
+        '}',
+        '',  # trailing newline
+    ])
+    with open(path, 'w') as f:
+        f.write('\n'.join(lines))
+
+
+def generate_resourcebuilder(directory, types, clients, modifiers, health_checks):
+    package_name = os.path.basename(directory)
+    path = os.path.join(directory, package_name + '.go')
+    lines = [
+        '// auto-generated with generate-lib-resources.py',
+        '',
+        '// Package {} reads supported objects from bytes.'.format(package_name),
+        'package {}'.format(package_name),
+        '',
+        'import (',
+        '\t"context"',
+        '\t"fmt"',
+        '',
+    ]
+
+    imports = {}
+    for import_name in [
+            'github.com/openshift/cluster-version-operator/lib',
+            'github.com/openshift/cluster-version-operator/lib/resourceapply',
+            'github.com/openshift/cluster-version-operator/lib/resourceread',
+            'k8s.io/client-go/rest',
+            ]:
+        imports[import_name] = '\t"{}"'.format(import_name)
+
+    ignored_packages = set()
+
+    for package in types.keys():
+        if not clients.get(package):
+            ignored_packages.add(package)
+            continue
+        base, version = os.path.split(package)
+        short_name = os.path.basename(base)
+        imports[package] = '\t{}{} "{}"'.format(short_name, version, package)
+
+    client_properties = {}
+    for package, client in clients.items():
+        if package in ignored_packages:
+            continue
+        base, version = os.path.split(client['package'])
+        short_name = os.path.basename(base)
+        client_short_name = '{}client{}'.format(short_name, version)
+        imports[client['package']] = '\t{} "{}"'.format(client_short_name, client['package'])
+        client_properties['{}Client{}'.format(short_name, version)] = {
+            'package': package,
+            'client_short_name': client_short_name,
+            'type': '*{}.{}'.format(client_short_name, client['type']),
+            'protobuf': client['package'].startswith('k8s.io/') and 'kube-aggregator' not in client['package'],
+        }
+
+    lines.extend([import_line for _, import_line in sorted(imports.items(), key=lambda package_line: package_line[0])])
+
+    longest_property = max(len(prop_name) for prop_name in client_properties.keys())
+
+    lines.extend([
+        ')',
+        '',
+        '// builder manages single-manifest cluster reconciliation and monitoring.',
+        'type builder struct {',
+        '\traw      []byte',
+        '\tmode     Mode',
+        '\tmodifier MetaV1ObjectModifierFunc',
+        '',
+    ])
+    lines.extend([
+        '\t{:{width}} {}'.format(prop_name, data['type'], width=longest_property)
+        for prop_name, data in sorted(client_properties.items())
+    ])
+
+    lines.extend([
+        '}',
+        '',
+        'func newBuilder(config *rest.Config, m lib.Manifest) Interface {',
+        '\treturn &builder{',
+        '\t\traw: m.Raw,',
+        '',
+    ])
+    for prop_name, data in sorted(client_properties.items()):
+        new_client_arg = 'config'
+        if data.get('protobuf'):
+            new_client_arg = 'withProtobuf({})'.format(new_client_arg)
+        lines.append('\t\t{:{width}} {}.NewForConfigOrDie({}),'.format(prop_name + ':', data['client_short_name'], new_client_arg, width=longest_property+1))
+
+    lines.extend([
+        '\t}',
+        '}',
+        '',
+        'func (b *builder) WithMode(m Mode) Interface {',
+        '\tb.mode = m',
+        '\treturn b',
+        '}',
+        '',
+        'func (b *builder) WithModifier(f MetaV1ObjectModifierFunc) Interface {',
+        '\tb.modifier = f',
+        '\treturn b',
+        '}',
+        '',
+        'func (b *builder) Do(ctx context.Context) error {',
+        '\tobj := resourceread.ReadOrDie(b.raw)',
+        '',
+        '\tswitch typedObject := obj.(type) {'
+    ])
+
+    for package, type_names in sorted(types.items()):
+        if package in ignored_packages:
+            continue
+        base, version = os.path.split(package)
+        short_name = os.path.basename(base)
+        try:
+            client_prop_name = [key for key, data in client_properties.items() if data['package'] == package][0]
+        except IndexError as error:
+            raise ValueError('no client property found for {}'.format(package))
+        for type_name in sorted(type_names):
+            lines.extend([
+                '\tcase *{}{}.{}:'.format(short_name, version, type_name),
+                '\t\tif b.modifier != nil {',
+                '\t\t\tb.modifier(typedObject)',
+                '\t\t}',
+            ])
+            type_key = (package, type_name)
+            modifier = modifiers.get(type_key)
+            if modifier:
+                lines.extend([
+                    '\t\tif err := {}(ctx, typedObject); err != nil {{'.format(modifier),
+                    '\t\t\treturn err',
+                    '\t\t}',
+                ])
+            lines.extend([
+                '\t\tif _, _, err := resourceapply.Apply{}{}(ctx, b.{}, typedObject); err != nil {{'.format(type_name, version, client_prop_name),
+                '\t\t\treturn err',
+                '\t\t}',
+            ])
+            health_check = health_checks.get(type_key)
+            if health_check:
+                lines.append('\t\treturn {}(ctx, typedObject)'.format(health_check))
+
+    lines.extend([
+        '\tdefault:',
+        '\t\treturn fmt.Errorf("unrecognized manifest type: %T", obj)',
+        '\t}',
+        '',
+        '\treturn nil',
+        '}',
+        '',
+        'func init() {',
+        '\trm := NewResourceMapper()',
+    ])
+
+    for sgv, data in sorted(scheme_group_versions(types=types).items()):
+        if data['package'] in ignored_packages:
+            continue
+        for type_name in sorted(data['types']):
+            lines.append('\trm.RegisterGVK({}.WithKind("{}"), newBuilder)'.format(sgv, type_name))
+
+    lines.extend([
+        '\trm.AddToMap(Mapper)',
+        '}',
+        '',  # trailing newline
+    ])
+
+    with open(path, 'w') as f:
+        f.write('\n'.join(lines))
+
+
+def scheme_group_versions(types):
+    sgvs = {}
+    for package, type_names in types.items():
+        base, version = os.path.split(package)
+        short_name = os.path.basename(base)
+        sgv = '{}{}.SchemeGroupVersion'.format(short_name, version)
+        sgvs[sgv] = {
+            'package': package,
+            'short_name': short_name,
+            'types': type_names,
+            'version': version,
+        }
+    return sgvs
+
+
+if __name__ == '__main__':
+    types = {
+        'github.com/openshift/api/image/v1': {'ImageStream'}, # for payload loading
+        'github.com/openshift/api/security/v1': {'SecurityContextConstraints'},
+        'k8s.io/api/apps/v1': {'DaemonSet', 'Deployment'},
+        'k8s.io/api/batch/v1': {'Job'},
+        'k8s.io/api/core/v1': {'ConfigMap', 'Namespace', 'Service', 'ServiceAccount'},
+        'k8s.io/api/rbac/v1': {'ClusterRole', 'ClusterRoleBinding', 'Role', 'RoleBinding'},
+        'k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1': {'CustomResourceDefinition'},
+        'k8s.io/kube-aggregator/pkg/apis/apiregistration/v1': {'APIService'},
+    }
+
+    types['k8s.io/api/rbac/v1beta1'] = types['k8s.io/api/rbac/v1']
+    types['k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1'] = types['k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1']
+    types['k8s.io/kube-aggregator/pkg/apis/apiregistration/v1beta1'] = types['k8s.io/kube-aggregator/pkg/apis/apiregistration/v1']
+
+    clients = {
+        'github.com/openshift/api/security/v1': {'package': 'github.com/openshift/client-go/security/clientset/versioned/typed/security/v1', 'type': 'SecurityV1Client'},
+        'github.com/openshift/api/config/v1': {'package': 'github.com/openshift/client-go/config/clientset/versioned/typed/config/v1', 'type': 'ConfigV1Client'},
+        'k8s.io/api/apps/v1': {'package': 'k8s.io/client-go/kubernetes/typed/apps/v1', 'type': 'AppsV1Client'},
+        'k8s.io/api/batch/v1': {'package': 'k8s.io/client-go/kubernetes/typed/batch/v1', 'type': 'BatchV1Client'},
+        'k8s.io/api/core/v1': {'package': 'k8s.io/client-go/kubernetes/typed/core/v1', 'type': 'CoreV1Client'},
+        'k8s.io/api/rbac/v1': {'package': 'k8s.io/client-go/kubernetes/typed/rbac/v1', 'type': 'RbacV1Client'},
+        'k8s.io/api/rbac/v1beta1': {'package': 'k8s.io/client-go/kubernetes/typed/rbac/v1beta1', 'type': 'RbacV1beta1Client'},
+        'k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1': {'package': 'k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1', 'type': 'ApiextensionsV1Client'},
+        'k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1': {'package': 'k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1beta1', 'type': 'ApiextensionsV1beta1Client'},
+        'k8s.io/kube-aggregator/pkg/apis/apiregistration/v1': {'package': 'k8s.io/kube-aggregator/pkg/client/clientset_generated/clientset/typed/apiregistration/v1', 'type': 'ApiregistrationV1Client'},
+        'k8s.io/kube-aggregator/pkg/apis/apiregistration/v1beta1': {'package': 'k8s.io/kube-aggregator/pkg/client/clientset_generated/clientset/typed/apiregistration/v1beta1', 'type': 'ApiregistrationV1beta1Client'},
+    }
+
+    modifiers = {
+        ('k8s.io/api/apps/v1', 'Deployment'): 'b.modifyDeployment',
+        ('k8s.io/api/apps/v1', 'DaemonSet'): 'b.modifyDaemonSet',
+    }
+
+    health_checks = {
+        ('k8s.io/api/apps/v1', 'Deployment'): 'b.checkDeploymentHealth',
+        ('k8s.io/api/apps/v1', 'DaemonSet'): 'b.checkDaemonSetHealth',
+        ('k8s.io/api/batch/v1', 'Job'): 'b.checkJobHealth',
+    }
+
+    generate_lib_resources(directory='lib', types=types, clients=clients, modifiers=modifiers, health_checks=health_checks)

lib/resourcebuilder/apiext.go
@@ -1,64 +1,0 @@
-package resourcebuilder
-
-import (
-	"context"
-	"fmt"
-
-	"github.com/openshift/cluster-version-operator/lib"
-	"github.com/openshift/cluster-version-operator/lib/resourceapply"
-	"github.com/openshift/cluster-version-operator/lib/resourceread"
-	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	apiextv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
-	apiextclientv1 "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1"
-	apiextclientv1beta1 "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1beta1"
-	"k8s.io/client-go/rest"
-)
-
-type crdBuilder struct {
-	raw           []byte
-	modifier      MetaV1ObjectModifierFunc
-	clientV1beta1 *apiextclientv1beta1.ApiextensionsV1beta1Client
-	clientV1      *apiextclientv1.ApiextensionsV1Client
-}
-
-func newCRDBuilder(config *rest.Config, m lib.Manifest) Interface {
-	return &crdBuilder{
-		raw:           m.Raw,
-		clientV1beta1: apiextclientv1beta1.NewForConfigOrDie(withProtobuf(config)),
-		clientV1:      apiextclientv1.NewForConfigOrDie(withProtobuf(config)),
-	}
-}
-
-func (b *crdBuilder) WithMode(m Mode) Interface {
-	return b
-}
-
-func (b *crdBuilder) WithModifier(f MetaV1ObjectModifierFunc) Interface {
-	b.modifier = f
-	return b
-}
-
-func (b *crdBuilder) Do(ctx context.Context) error {
-	crd := resourceread.ReadCustomResourceDefinitionOrDie(b.raw)
-
-	switch typedCRD := crd.(type) {
-	case *apiextv1beta1.CustomResourceDefinition:
-		if b.modifier != nil {
-			b.modifier(typedCRD)
-		}
-		if _, _, err := resourceapply.ApplyCustomResourceDefinitionV1beta1(ctx, b.clientV1beta1, typedCRD); err != nil {
-			return err
-		}
-	case *apiextv1.CustomResourceDefinition:
-		if b.modifier != nil {
-			b.modifier(typedCRD)
-		}
-		if _, _, err := resourceapply.ApplyCustomResourceDefinitionV1(ctx, b.clientV1, typedCRD); err != nil {
-			return err
-		}
-	default:
-		return fmt.Errorf("unrecognized CustomResourceDefinition version: %T", crd)
-	}
-
-	return nil
-}

commit 6e12b9f239db497170306659c3d934545b28ec4a

lib/resourcebuilder/apiext: Restore check for Established=True CRDs
I'd dropped this in cc9292a061 (lib/resourcebuilder: Replace wait-for
with single-shot "is it alive now?", 2020-07-30, #400), claiming:

  There's no object status for CRDs or DaemonSets that marks "we are
  really hurting".  The v1.18.0 Kubernetes CRD and DaemonSet
  controllers do not set any conditions in their operand status
  (although the API for those conditions exists [2,3]).  With this
  commit, we have very minimal wait logic for either.  Sufficiently
  unhealthy DaemonSet should be reported on via their associated
  ClusterOperator, and sufficiently unhealthy CRD should be reported
  on when we fail to push any custom resources consuming them
  (Task.Run retries will give the API server time to ready itself
  after accepting a CRD update before the CVO fails its sync cycle).

But from [1]:

  > It might take a few seconds for the endpoint to be created. You
  > can watch the Established condition of your
  > CustomResourceDefinition to be true or watch the discovery
  > information of the API server for your resource to show up.

So I was correct that we will hear about CRD issues when we fail to
push a dependent custom resource.  But I was not correct in claiming
that the CRD controller set no conditions.  I was probably confused by
e8ffccb878 (lib: Add autogeneration for some resource* functionality,
2020-07-29, #420), which broke the health-check inputs as described in
002591daf8 (lib/resourcebuilder: Use actual resource in check*Health
calls, 2022-05-03, #771).  The code I removed in cc9292a061 was in
fact looking at the Established condition already.

This commit restores the Established check, but without the previous
PollImmediateUntil wait.

[1]: https://kubernetes.io/docs/tasks/extend-kubernetes/custom-resources/custom-resource-definitions/#create-a-customresourcedefinition

hack/generate-lib-resources.py
@@ -306,9 +306,10 @@ if __name__ == '__main__':
     }
 
     health_checks = {
-        ('k8s.io/api/apps/v1', 'Deployment'): 'b.checkDeploymentHealth',
         ('k8s.io/api/apps/v1', 'DaemonSet'): 'b.checkDaemonSetHealth',
+        ('k8s.io/api/apps/v1', 'Deployment'): 'b.checkDeploymentHealth',
         ('k8s.io/api/batch/v1', 'Job'): 'b.checkJobHealth',
+        ('k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1', 'CustomResourceDefinition'): 'b.checkCustomResourceDefinitionHealth',
     }
 
     generate_lib_resources(directory='lib', types=types, clients=clients, modifiers=modifiers, health_checks=health_checks)

lib/resourcebuilder/apiext.go
@@ -1,0 +1,25 @@
+package resourcebuilder
+
+import (
+	"context"
+	"fmt"
+
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+)
+
+func (b *builder) checkCustomResourceDefinitionHealth(ctx context.Context, crd *apiextv1.CustomResourceDefinition) error {
+	if b.mode == InitializingMode {
+		return nil
+	}
+
+	for _, condition := range crd.Status.Conditions {
+		if condition.Type == apiextv1.Established {
+			if condition.Status == apiextv1.ConditionTrue {
+				return nil
+			}
+			return fmt.Errorf("CustomResourceDefinition %s Established=%s, %s: %s", crd.Name, condition.Status, condition.Reason, condition.Message)
+		}
+	}
+
+	return fmt.Errorf("CustomResourceDefinition %s does not declare an Established status condition: %v", crd.Name, crd.Status.Conditions)
+}

lib/resourcebuilder/resourcebuilder.go
@@ -255,8 +255,10 @@ func (b *builder) Do(ctx context.Context) error {
 			updatingMode); err != nil {
 			return err
 		} else if !deleteReq {
-			if _, _, err := resourceapply.ApplyCustomResourceDefinitionv1(ctx, b.apiextensionsClientv1, typedObject, reconcilingMode); err != nil {
+			if actual, _, err := resourceapply.ApplyCustomResourceDefinitionv1(ctx, b.apiextensionsClientv1, typedObject, reconcilingMode); err != nil {
 				return err
+			} else if actual != nil {
+				return b.checkCustomResourceDefinitionHealth(ctx, actual)
 			}
 		}
 	default: