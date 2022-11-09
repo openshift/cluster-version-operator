0d8d8ecacb471cc21e67bc8b62ebbec586d47eaa
David Hurta <dhurta@redhat.com>
2022-11-09 16:02:38 +0100

    lib: Add /apis/operators.coreos.com/v1/operatorgroups handling
    Add /apis/operators.coreos.com/v1/operatorgroups handling that enables
    a custom way of processing manifests representing the OperatorGroup
    resource. Currently, the OperatorGroup resources are reconciled as
    generic resources. This doesn't provide defaulting of the fields.
    
    This commit will provide more precise processing of the OperatorGroup
    resources and will provide defaulting of the resources, which will fix
    the current hotlooping of the CVO.
    
    Currently, the CVO hotloops on the OperatorGroup resources due to the
    missing defaulting of the resource (Bug 2089093 [1]).
    
    The [2] commit introduced the UpgradeStrategy field to the OperatorGroup
    type. This field has a default value of "Default". The CVO currently
    doesn't know of this default value, and thus the CVO hotloops when
    handling the OperatorGroup resource as a generic resource because some
    manifests don't specify the value of the field and rely on
    defaulting [3].
    
    This commit will provide a more precise way of processing the
    OperatorGroup type and will also fix the mentioned bug by providing
    the defaulting for the resource.
    
    [1] https://bugzilla.redhat.com/show_bug.cgi?id=2089093
    [2] https://github.com/openshift/operator-framework-olm/commit/d895574f25098093ccde2108c7cb8e288972f1d7
    [3] https://github.com/openshift/operator-framework-olm/blob/ab5ecfd7f235142f191d91d103afabccdfd6ff04/manifests/0000_50_olm_13-operatorgroup-default.yaml

---
+++ lib/resourcedelete/operators.go
@@ -0,0 +1,39 @@
+package resourcedelete
+
+import (
+	"context"
+	"fmt"
+
+	operatorsv1 "github.com/operator-framework/api/pkg/operators/v1"
+	operatorsclientv1 "github.com/operator-framework/operator-lifecycle-manager/pkg/api/client/clientset/versioned/typed/operators/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// DeleteOperatorGroupv1 checks the given resource for a valid delete annotation. If found
+// it checks the status of a previously issued delete request. If delete has not been
+// requested and in UpdatingMode it will issue a delete request.
+func DeleteOperatorGroupv1(ctx context.Context, client operatorsclientv1.OperatorGroupsGetter, required *operatorsv1.OperatorGroup,
+	updateMode bool) (bool, error) {
+
+	if delAnnoFound, err := ValidDeleteAnnotation(required.Annotations); !delAnnoFound || err != nil {
+		return delAnnoFound, err
+	}
+	existing, err := client.OperatorGroups(required.Namespace).Get(ctx, required.Name, metav1.GetOptions{})
+	resource := Resource{
+		Kind:      "operatorgroup",
+		Namespace: required.Namespace,
+		Name:      required.Name,
+	}
+	if deleteRequested, err := GetDeleteProgress(resource, err); err == nil {
+		// Only request deletion when in update mode.
+		if !deleteRequested && updateMode {
+			if err := client.OperatorGroups(required.Namespace).Delete(ctx, required.Name, metav1.DeleteOptions{}); err != nil {
+				return true, fmt.Errorf("Delete request for %s failed, err=%v", resource, err)
+			}
+			SetDeleteRequested(existing, resource)
+		}
+	} else {
+		return true, fmt.Errorf("Error running delete for %s, err=%v", resource, err)
+	}
+	return true, nil
+}
---
+++ lib/resourcemerge/operators_test.go
@@ -0,0 +1,235 @@
+package resourcemerge
+
+import (
+	"testing"
+
+	operatorsv1 "github.com/operator-framework/api/pkg/operators/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
+)
+
+func TestEnsureOperatorGroup(t *testing.T) {
+	labelSelector := metav1.LabelSelector{}
+	tests := []struct {
+		name     string
+		existing operatorsv1.OperatorGroup
+		required operatorsv1.OperatorGroup
+
+		expectedModified bool
+		expected         operatorsv1.OperatorGroup
+	}{
+		{
+			name:     "existing-selector-nil-required-selector-non-nil",
+			existing: operatorsv1.OperatorGroup{},
+			required: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					Selector: &labelSelector}},
+
+			expectedModified: true,
+			expected: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					Selector: &labelSelector}},
+		},
+		{
+			name: "same target namespaces",
+			existing: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					TargetNamespaces: []string{"foo", "bar"},
+				},
+			},
+			required: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					TargetNamespaces: []string{"foo", "bar"},
+				},
+			},
+
+			expectedModified: false,
+			expected: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					TargetNamespaces: []string{"foo", "bar"},
+				},
+			},
+		},
+		{
+			name: "different target namespaces",
+			existing: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					TargetNamespaces: []string{},
+				},
+			},
+			required: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					TargetNamespaces: []string{"foo", "bar"},
+				},
+			},
+
+			expectedModified: true,
+			expected: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					TargetNamespaces: []string{"foo", "bar"},
+				},
+			},
+		},
+		{
+			name: "same service account names",
+			existing: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					ServiceAccountName: "foo",
+				},
+			},
+			required: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					ServiceAccountName: "foo",
+				},
+			},
+
+			expectedModified: false,
+			expected: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					ServiceAccountName: "foo",
+				},
+			},
+		},
+		{
+			name: "different service account names",
+			existing: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					ServiceAccountName: "foo",
+				},
+			},
+			required: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					ServiceAccountName: "bar",
+				},
+			},
+
+			expectedModified: true,
+			expected: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					ServiceAccountName: "bar",
+				},
+			},
+		},
+		{
+			name: "same static provided apis",
+			existing: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					StaticProvidedAPIs: true,
+				},
+			},
+			required: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					StaticProvidedAPIs: true,
+				},
+			},
+
+			expectedModified: false,
+			expected: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					StaticProvidedAPIs: true,
+				},
+			},
+		},
+		{
+			name: "different static provided apis",
+			existing: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					StaticProvidedAPIs: true,
+				},
+			},
+			required: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					StaticProvidedAPIs: false,
+				},
+			},
+
+			expectedModified: true,
+			expected: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					StaticProvidedAPIs: false,
+				},
+			},
+		},
+		{
+			name: "implicit upgrade strategy",
+			existing: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					UpgradeStrategy: operatorsv1.UpgradeStrategyUnsafeFailForward,
+				},
+			},
+			required: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{},
+			},
+
+			expectedModified: true,
+			expected: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					UpgradeStrategy: operatorsv1.UpgradeStrategyDefault,
+				},
+			},
+		},
+		{
+			name: "same upgrade strategy",
+			existing: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					UpgradeStrategy: operatorsv1.UpgradeStrategyUnsafeFailForward,
+				},
+			},
+			required: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					UpgradeStrategy: operatorsv1.UpgradeStrategyUnsafeFailForward,
+				},
+			},
+
+			expectedModified: false,
+			expected: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					UpgradeStrategy: operatorsv1.UpgradeStrategyUnsafeFailForward,
+				},
+			},
+		},
+		{
+			name: "different upgrade strategy",
+			existing: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					UpgradeStrategy: operatorsv1.UpgradeStrategyUnsafeFailForward,
+				},
+			},
+			required: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					UpgradeStrategy: operatorsv1.UpgradeStrategyDefault,
+				},
+			},
+
+			expectedModified: true,
+			expected: operatorsv1.OperatorGroup{
+				Spec: operatorsv1.OperatorGroupSpec{
+					UpgradeStrategy: operatorsv1.UpgradeStrategyDefault,
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			defaultOperatorGroup(&test.existing, test.existing)
+			defaultOperatorGroup(&test.expected, test.expected)
+			modified := pointer.Bool(false)
+			EnsureOperatorGroup(modified, &test.existing, test.required)
+			if *modified != test.expectedModified {
+				t.Errorf("mismatch modified got: %v want: %v", *modified, test.expectedModified)
+			}
+
+			if !equality.Semantic.DeepEqual(test.existing, test.expected) {
+				t.Errorf("mismatch OperatorGroup got: %v want: %v", test.existing, test.expected)
+			}
+		})
+	}
+}
+
+// Ensures the structure contains any defaults not explicitly set by the test
+func defaultOperatorGroup(in *operatorsv1.OperatorGroup, from operatorsv1.OperatorGroup) {
+	modified := pointer.Bool(false)
+	EnsureOperatorGroup(modified, in, from)
+}
