0e6c916f99e05983190202575bb530200560acb9
David Hurta <dhurta@redhat.com>
2022-11-30 17:06:48 +0100

    lib/resourcemerge: Fix removing caBundle field of CRDs when "inject-cabundle=true"
    This commit will fix CVO's behavior of removing the `caBundle` field of
    CRDs when reconciling manifests as described in the bug [1].
    
    Currently, the users can omit the `caBundle` field in the CRDs and set
    the `service.beta.openshift.io/inject-cabundle=true` annotation so that
    the given `caBundle` field is managed by the service-ca controller
    (this functionality is not limited only to CRDs) [2].
    
    However, the CVO is not aware of this feature. CVO, when reconciling
    such CRD, sees an empty `caBundle` field in the given release manifest,
    and CVO then removes any value in the existing applied CRD so it matches
    the release manifest semantically.
    
    This commit makes the CVO look into the existing resource and check for
    the `service.beta.openshift.io/inject-cabundle=true` annotation. If it's
    set then the service-ca controller is responsible for the field, and CVO
    will ignore any differences between the existing `caBundle` field and
    the `caBundle` field in the given release manifest of the CRD.
    
    This bug also caused the CVO to hotloop on CRDs as mentioned in the bug [3].
    
    Even though the `.Spec.Conversion.Webhook.ClientConfig` is required to
    be set if `.Spec.Conversion.Strategy` is set to "Webhook", check for its
    existence. A developer might have written an incorrect YAML manifest
    which could result in CVO looking at `nil` address, resulting in a
    panic. Check for nil pointers in the `existing` instance as the instance
    could have been rewritten entirely by the user.
    
    Add a new file for the new constant `injectCABundleAnnotation` since the
    constant can be used across multiple files in the `resourcemerge`
    package. The annotation can be used in multiple groups for different
    kinds (CRD, API service, webhook configurations) [2]. Don't export the
    constant because its use is only intended for the `resourcemerge`
    package.
    
    [1] https://issues.redhat.com/browse/OCPBUGS-702
    [2] https://docs.openshift.com/container-platform/4.11/security/certificates/service-serving-certificate.html
    [3] https://bugzilla.redhat.com/show_bug.cgi?id=2089097

--- lib/resourcemerge/apiext.go
+++ lib/resourcemerge/apiext.go
@@ -12,18 +12,45 @@ import (
 // modified is set to true when existing had to be updated with required.
 func EnsureCustomResourceDefinitionV1(modified *bool, existing *apiextv1.CustomResourceDefinition, required apiextv1.CustomResourceDefinition) {
 	EnsureObjectMeta(modified, &existing.ObjectMeta, required.ObjectMeta)
+	ensureCustomResourceDefinitionV1Defaults(&required)
+	ensureCustomResourceDefinitionV1CaBundle(&required, *existing)
+	if !equality.Semantic.DeepEqual(existing.Spec, required.Spec) {
+		*modified = true
+		existing.Spec = required.Spec
+	}
+}
 
-	// apply defaults to blue print
+func ensureCustomResourceDefinitionV1Defaults(required *apiextv1.CustomResourceDefinition) {
 	if len(required.Spec.Names.Singular) == 0 {
 		required.Spec.Names.Singular = strings.ToLower(required.Spec.Names.Kind)
 	}
 	if len(required.Spec.Names.ListKind) == 0 {
 		required.Spec.Names.ListKind = required.Spec.Names.Kind + "List"
 	}
+}
 
-	// we stomp everything
-	if !equality.Semantic.DeepEqual(existing.Spec, required.Spec) {
-		*modified = true
-		existing.Spec = required.Spec
+// ensureCustomResourceDefinitionV1CaBundle ensures that the field
+// spec.Conversion.Webhook.ClientConfig.CABundle of a CRD is not managed by the CVO when
+// the service-ca controller is responsible for the field.
+func ensureCustomResourceDefinitionV1CaBundle(required *apiextv1.CustomResourceDefinition, existing apiextv1.CustomResourceDefinition) {
+	if val, ok := existing.ObjectMeta.Annotations[injectCABundleAnnotation]; !ok || val != "true" {
+		return
+	}
+	req := required.Spec.Conversion
+	if req == nil ||
+		req.Webhook == nil ||
+		req.Webhook.ClientConfig == nil {
+		return
+	}
+	if req.Strategy != apiextv1.WebhookConverter {
+		// The service CA bundle is only injected by the service-ca controller into
+		// the CRD if the CRD is configured to use a webhook for conversion
+		return
+	}
+	exc := existing.Spec.Conversion
+	if exc != nil &&
+		exc.Webhook != nil &&
+		exc.Webhook.ClientConfig != nil {
+		req.Webhook.ClientConfig.CABundle = exc.Webhook.ClientConfig.CABundle
 	}
 }
---
+++ lib/resourcemerge/helper.go
@@ -0,0 +1,5 @@
+package resourcemerge
+
+// injectCABundleAnnotation is the annotation used to indicate into which resources
+// the service-ca controller should inject the CA bundle.
+const injectCABundleAnnotation = "service.beta.openshift.io/inject-cabundle"
