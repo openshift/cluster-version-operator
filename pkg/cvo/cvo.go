9ac845fb43a8a066aef84b78251820fb97f42667
W. Trevor King <wking@tremily.us>
2023-03-25 16:47:21 -0700

    pkg/payload/precondition/clusterversion/rollback: New precondition
    We have occasional cases where admins attempt a rollback, despite
    long-standing docs [1]:
    
      Only upgrading to a newer version is supported. Reverting or rolling
      back your cluster to a previous version is not supported. If your
      update fails, contact Red Hat support.
    
    Deeper history for that content includes [2,3,4].  With this commit,
    we'll refuse to accept rollbacks unless the administrator sets 'Force'
    to waive our guards.
    
    [1]: https://docs.openshift.com/container-platform/4.12/updating/understanding-openshift-updates.html
    [2]: https://github.com/openshift/openshift-docs/blame/60ae4bf339756189b7e72491b79d53764cbe85fa/modules/update-service-overview.adoc#L33
    [3]: https://github.com/openshift/openshift-docs/pull/31432/commits/10515fe97929f5a9c344f8e519f823f941ed7e5c
    [4]: https://github.com/openshift/openshift-docs/pull/15118/commits/7c882ea47c63468b248b7b2ea7fab7c550883932#diff-769155bafe6ff5307aa01d79f6b07ad34bc72fd8431fef3501a554802d653ee5R52-R53

--- pkg/cvo/cvo.go
+++ pkg/cvo/cvo.go
@@ -923,6 +923,7 @@ func hasReachedLevel(cv *configv1.ClusterVersion, desired configv1.Update) bool 
 
 func (optr *Operator) defaultPreconditionChecks() precondition.List {
 	return []precondition.Precondition{
+		preconditioncv.NewRollback(optr.currentVersion),
 		preconditioncv.NewUpgradeable(optr.cvLister),
 		preconditioncv.NewRecentEtcdBackup(optr.cvLister, optr.coLister),
 		preconditioncv.NewRecommendedUpdate(optr.cvLister),
