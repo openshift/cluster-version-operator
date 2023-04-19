9a309d30aa8d1871beea2e3e134998cba0830e39
David Hurta <dhurta@redhat.com>
2023-04-19 16:15:34 +0200

    lib: Update applying Job resources
    Although there is currently no hotlooping present when applying Job
    resources, it is necessary to update it to fix hotlooping on CronJob
    resources.
    
    Some of the hotlooping on CronJob resources is caused by hotlooping on
    the `CronJob.CronJobSpec.JobTemplate.Spec`field. To ensure it's
    correctly implemented and to properly reuse existing code, this commit
    updates applying of Job resources because its logic can be then reused
    in handling CronJob resources. This also fixes the applying of Job
    resources to prevent any hotlooping caused by missing defaulting and
    missing applying for new fields in the future.
    
    The default values were set by following the Kubernetes documentation [1].
    
    [1] https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.26/#job-v1-batch

--- lib/resourcemerge/batch.go
+++ lib/resourcemerge/batch.go
@@ -2,6 +2,8 @@ package resourcemerge
 
 import (
 	batchv1 "k8s.io/api/batch/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/utils/pointer"
 )
 
 // EnsureJob ensures that the existing matches the required.
@@ -15,10 +17,34 @@ func ensureJobSpec(modified *bool, existing *batchv1.JobSpec, required batchv1.J
 	if required.Selector != nil {
 		panic("spec.selector is not supported in Job manifests")
 	}
+	ensureJobSpecDefault(&required)
 	setInt32Ptr(modified, &existing.Parallelism, required.Parallelism)
 	setInt32Ptr(modified, &existing.Completions, required.Completions)
 	setInt64Ptr(modified, &existing.ActiveDeadlineSeconds, required.ActiveDeadlineSeconds)
 	setInt32Ptr(modified, &existing.BackoffLimit, required.BackoffLimit)
 	setBoolPtr(modified, &existing.ManualSelector, required.ManualSelector)
+	setInt32Ptr(modified, &existing.TTLSecondsAfterFinished, required.TTLSecondsAfterFinished)
+	setBoolPtr(modified, &existing.Suspend, required.Suspend)
 	ensurePodTemplateSpec(modified, &existing.Template, required.Template)
+	if !equality.Semantic.DeepEqual(existing.CompletionMode, required.CompletionMode) {
+		*modified = true
+		existing.CompletionMode = required.CompletionMode
+	}
+	if !equality.Semantic.DeepEqual(existing.PodFailurePolicy, required.PodFailurePolicy) {
+		*modified = true
+		existing.PodFailurePolicy = required.PodFailurePolicy
+	}
+}
+
+func ensureJobSpecDefault(required *batchv1.JobSpec) {
+	if required.BackoffLimit == nil {
+		required.BackoffLimit = pointer.Int32(6)
+	}
+	if required.CompletionMode == nil {
+		completionMode := batchv1.NonIndexedCompletion
+		required.CompletionMode = &completionMode
+	}
+	if required.Suspend == nil {
+		required.Suspend = pointer.Bool(false)
+	}
 }
--- lib/resourcemerge/batch_test.go
+++ lib/resourcemerge/batch_test.go
@@ -2,63 +2,408 @@ package resourcemerge
 
 import (
 	"testing"
+	"time"
 
+	"github.com/google/go-cmp/cmp"
 	batchv1 "k8s.io/api/batch/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/pointer"
 )
 
-func TestEnsureJob(t *testing.T) {
-	labelSelector := metav1.LabelSelector{}
+func TestEnsureJob_JobStatus(t *testing.T) {
 	tests := []struct {
 		name     string
-		existing batchv1.Job
-		required batchv1.Job
-
-		expectedModified bool
-		expectedPanic    bool
-		expected         batchv1.Job
+		existing batchv1.JobStatus
+		required batchv1.JobStatus
 	}{
 		{
-			name: "different backofflimit count",
-			existing: batchv1.Job{
-				Spec: batchv1.JobSpec{
-					BackoffLimit: pointer.Int32(2)}},
-			required: batchv1.Job{
-				Spec: batchv1.JobSpec{
-					BackoffLimit: pointer.Int32(3)}},
+			name: "cvo should ignore same status field",
+			existing: batchv1.JobStatus{
+				StartTime: &metav1.Time{Time: time.Unix(0, 0)},
+				Active:    3,
+				Succeeded: 4,
+				Failed:    2,
+				Ready:     pointer.Int32(1),
+			},
+			required: batchv1.JobStatus{
+				StartTime: &metav1.Time{Time: time.Unix(0, 0)},
+				Active:    3,
+				Succeeded: 4,
+				Failed:    2,
+				Ready:     pointer.Int32(1),
+			},
+		},
+		{
+			name: "cvo should ignore different status field",
+			existing: batchv1.JobStatus{
+				StartTime: &metav1.Time{Time: time.Unix(0, 0)},
+				Active:    3,
+				Succeeded: 4,
+				Failed:    2,
+				Ready:     pointer.Int32(1),
+			},
+			required: batchv1.JobStatus{},
+		},
+	}
 
+	var expected batchv1.Job
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			existing := batchv1.Job{Status: test.existing}
+			required := batchv1.Job{Status: test.required}
+			defaultJob(&existing, existing)
+			existing.DeepCopyInto(&expected)
+			modified := pointer.Bool(false)
+			EnsureJob(modified, &existing, required)
+			if *modified != false {
+				t.Errorf("mismatch modified got: %v want: %v", *modified, false)
+			}
+			if !equality.Semantic.DeepEqual(existing, expected) {
+				t.Errorf("unexpected: %s", cmp.Diff(expected, existing))
+			}
+		})
+	}
+}
+
+func TestEnsureJob_JobSpec(t *testing.T) {
+	NonIndexedCompletion := batchv1.NonIndexedCompletion
+	IndexedCompletion := batchv1.IndexedCompletion
+	tests := []struct {
+		name     string
+		existing batchv1.JobSpec
+		required batchv1.JobSpec
+
+		expectedModified bool
+	}{
+		{
+			name: "same parallelism",
+			existing: batchv1.JobSpec{
+				Parallelism: pointer.Int32(10),
+			},
+			required: batchv1.JobSpec{
+				Parallelism: pointer.Int32(10),
+			},
+			expectedModified: false,
+		},
+		{
+			name: "different parallelism",
+			existing: batchv1.JobSpec{
+				Parallelism: pointer.Int32(10),
+			},
+			required: batchv1.JobSpec{
+				Parallelism: pointer.Int32(5),
+			},
 			expectedModified: true,
-			expected: batchv1.Job{
-				Spec: batchv1.JobSpec{
-					BackoffLimit: pointer.Int32(3)}},
+		},
+		{
+			name: "same completions",
+			existing: batchv1.JobSpec{
+				Completions: pointer.Int32(10),
+			},
+			required: batchv1.JobSpec{
+				Completions: pointer.Int32(10),
+			},
+			expectedModified: false,
+		},
+		{
+			name: "different completions",
+			existing: batchv1.JobSpec{
+				Completions: pointer.Int32(10),
+			},
+			required: batchv1.JobSpec{
+				Completions: pointer.Int32(5),
+			},
+			expectedModified: true,
+		},
+		{
+			name: "same active deadline seconds",
+			existing: batchv1.JobSpec{
+				ActiveDeadlineSeconds: pointer.Int64(10),
+			},
+			required: batchv1.JobSpec{
+				ActiveDeadlineSeconds: pointer.Int64(10),
+			},
+			expectedModified: false,
+		},
+		{
+			name: "different active deadline seconds",
+			existing: batchv1.JobSpec{
+				ActiveDeadlineSeconds: pointer.Int64(10),
+			},
+			required: batchv1.JobSpec{
+				ActiveDeadlineSeconds: pointer.Int64(5),
+			},
+			expectedModified: true,
+		},
+		{
+			name: "same pod failure policy",
+			existing: batchv1.JobSpec{
+				PodFailurePolicy: &batchv1.PodFailurePolicy{
+					Rules: []batchv1.PodFailurePolicyRule{
+						{
+							Action: batchv1.PodFailurePolicyActionIgnore,
+							OnPodConditions: []batchv1.PodFailurePolicyOnPodConditionsPattern{
+								{
+									Type:   v1.PodReady,
+									Status: metav1.StatusSuccess,
+								},
+							},
+						},
+					},
+				},
+			},
+			required: batchv1.JobSpec{
+				PodFailurePolicy: &batchv1.PodFailurePolicy{
+					Rules: []batchv1.PodFailurePolicyRule{
+						{
+							Action: batchv1.PodFailurePolicyActionIgnore,
+							OnPodConditions: []batchv1.PodFailurePolicyOnPodConditionsPattern{
+								{
+									Type:   v1.PodReady,
+									Status: metav1.StatusSuccess,
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedModified: false,
+		},
+		{
+			name: "different pod failure policy",
+			existing: batchv1.JobSpec{
+				PodFailurePolicy: &batchv1.PodFailurePolicy{
+					Rules: []batchv1.PodFailurePolicyRule{
+						{
+							Action: batchv1.PodFailurePolicyActionIgnore,
+							OnPodConditions: []batchv1.PodFailurePolicyOnPodConditionsPattern{
+								{
+									Type:   v1.PodReady,
+									Status: metav1.StatusSuccess,
+								},
+							},
+						},
+					},
+				},
+			},
+			required:         batchv1.JobSpec{},
+			expectedModified: true,
 		},
 		{
 			name: "same backofflimit count",
-			existing: batchv1.Job{
-				Spec: batchv1.JobSpec{
-					BackoffLimit: pointer.Int32(2)}},
-			required: batchv1.Job{
-				Spec: batchv1.JobSpec{
-					BackoffLimit: pointer.Int32(2)}},
-
+			existing: batchv1.JobSpec{
+				BackoffLimit: pointer.Int32(2),
+			},
+			required: batchv1.JobSpec{
+				BackoffLimit: pointer.Int32(2),
+			},
 			expectedModified: false,
-			expected: batchv1.Job{
-				Spec: batchv1.JobSpec{
-					BackoffLimit: pointer.Int32(2)}},
 		},
 		{
-			name: "required-Selector not nil",
-			existing: batchv1.Job{
-				Spec: batchv1.JobSpec{
-					Selector:       &labelSelector,
-					ManualSelector: pointer.Bool(false)}},
-			required: batchv1.Job{
-				Spec: batchv1.JobSpec{
-					Selector:       &labelSelector,
-					ManualSelector: pointer.Bool(true)}},
+			name: "different backofflimit count",
+			existing: batchv1.JobSpec{
+				BackoffLimit: pointer.Int32(2),
+			},
+			required: batchv1.JobSpec{
+				BackoffLimit: pointer.Int32(3),
+			},
+			expectedModified: true,
+		},
+		{
+			name: "implicit backofflimit count",
+			existing: batchv1.JobSpec{
+				BackoffLimit: pointer.Int32(6),
+			},
+			required:         batchv1.JobSpec{},
+			expectedModified: false,
+		},
+		{
+			name: "same manual selector",
+			existing: batchv1.JobSpec{
+				ManualSelector: pointer.Bool(true),
+			},
+			required: batchv1.JobSpec{
+				ManualSelector: pointer.Bool(true),
+			},
+			expectedModified: false,
+		},
+		{
+			name: "different manual selector",
+			existing: batchv1.JobSpec{
+				ManualSelector: pointer.Bool(true),
+			},
+			required: batchv1.JobSpec{
+				ManualSelector: pointer.Bool(false),
+			},
+			expectedModified: true,
+		},
+		{
+			name: "same template",
+			existing: batchv1.JobSpec{
+				Template: v1.PodTemplateSpec{
+					Spec: v1.PodSpec{
+						Containers: []v1.Container{
+							{
+								Name:  "app",
+								Image: "app:latest",
+							},
+						},
+					},
+				},
+			},
+			required: batchv1.JobSpec{
+				Template: v1.PodTemplateSpec{
+					Spec: v1.PodSpec{
+						Containers: []v1.Container{
+							{
+								Name:  "app",
+								Image: "app:latest",
+							},
+						},
+					},
+				},
+			},
+			expectedModified: false,
+		},
+		{
+			name: "different template",
+			existing: batchv1.JobSpec{
+				Template: v1.PodTemplateSpec{
+					Spec: v1.PodSpec{
+						Containers: []v1.Container{
+							{
+								Name:  "app",
+								Image: "app:latest",
+							},
+						},
+					},
+				},
+			},
+			required:         batchv1.JobSpec{},
+			expectedModified: true,
+		},
+		{
+			name: "same TTL seconds after finished",
+			existing: batchv1.JobSpec{
+				TTLSecondsAfterFinished: pointer.Int32(10),
+			},
+			required: batchv1.JobSpec{
+				TTLSecondsAfterFinished: pointer.Int32(10),
+			},
+			expectedModified: false,
+		},
+		{
+			name: "different TTL seconds after finished",
+			existing: batchv1.JobSpec{
+				TTLSecondsAfterFinished: pointer.Int32(10),
+			},
+			required: batchv1.JobSpec{
+				TTLSecondsAfterFinished: pointer.Int32(5),
+			},
+			expectedModified: true,
+		},
+		{
+			name: "same completion mode",
+			existing: batchv1.JobSpec{
+				CompletionMode: &IndexedCompletion,
+			},
+			required: batchv1.JobSpec{
+				CompletionMode: &IndexedCompletion,
+			},
+			expectedModified: false,
+		},
+		{
+			name: "different completion mode",
+			existing: batchv1.JobSpec{
+				CompletionMode: &IndexedCompletion,
+			},
+			required: batchv1.JobSpec{
+				CompletionMode: &NonIndexedCompletion,
+			},
+			expectedModified: true,
+		},
+		{
+			name: "implicit completion mode",
+			existing: batchv1.JobSpec{
+				CompletionMode: &NonIndexedCompletion,
+			},
+			required:         batchv1.JobSpec{},
+			expectedModified: false,
+		},
+		{
+			name: "same suspend",
+			existing: batchv1.JobSpec{
+				Suspend: pointer.Bool(true),
+			},
+			required: batchv1.JobSpec{
+				Suspend: pointer.Bool(true),
+			},
+			expectedModified: false,
+		},
+		{
+			name: "different suspend",
+			existing: batchv1.JobSpec{
+				Suspend: pointer.Bool(true),
+			},
+			required: batchv1.JobSpec{
+				Suspend: pointer.Bool(false),
+			},
+			expectedModified: true,
+		},
+		{
+			name: "implicit suspend",
+			existing: batchv1.JobSpec{
+				Suspend: pointer.Bool(false),
+			},
+			required:         batchv1.JobSpec{},
+			expectedModified: false,
+		},
+	}
+	for _, test := range tests {
+		var expected batchv1.Job
+		t.Run(test.name, func(t *testing.T) {
+			existing := batchv1.Job{Spec: test.existing}
+			required := batchv1.Job{Spec: test.required}
+			if test.expectedModified == false {
+				existing.DeepCopyInto(&expected)
+			} else {
+				required.DeepCopyInto(&expected)
+			}
+			defaultJob(&existing, existing)
+			defaultJob(&expected, expected)
+			modified := pointer.Bool(false)
+			EnsureJob(modified, &existing, required)
+			if *modified != test.expectedModified {
+				t.Errorf("mismatch modified got: %v want: %v", *modified, test.expectedModified)
+			}
+			if !equality.Semantic.DeepEqual(existing, expected) {
+				t.Errorf("unexpected: %s", cmp.Diff(existing, expected))
+			}
+		})
+	}
+}
 
+func TestEnsureJob_JobSpec_Selector(t *testing.T) {
+	labelSelector := metav1.LabelSelector{}
+	tests := []struct {
+		name     string
+		existing batchv1.JobSpec
+		required batchv1.JobSpec
+
+		expectedPanic bool
+	}{
+		{
+			name: "required-Selector not nil",
+			existing: batchv1.JobSpec{
+				Selector:       &labelSelector,
+				ManualSelector: pointer.Bool(false),
+			},
+			required: batchv1.JobSpec{
+				Selector:       &labelSelector,
+				ManualSelector: pointer.Bool(true),
+			},
 			expectedPanic: true,
 		},
 	}
@@ -77,17 +422,11 @@ func TestEnsureJob(t *testing.T) {
 					}
 				}
 			}()
-			defaultJob(&test.existing, test.existing)
-			defaultJob(&test.expected, test.expected)
+			existing := batchv1.Job{Spec: test.existing}
+			required := batchv1.Job{Spec: test.required}
+			defaultJob(&existing, existing)
 			modified := pointer.Bool(false)
-			EnsureJob(modified, &test.existing, test.required)
-			if *modified != test.expectedModified {
-				t.Errorf("mismatch modified got: %v want: %v", *modified, test.expectedModified)
-			}
-
-			if !equality.Semantic.DeepEqual(test.existing, test.expected) {
-				t.Errorf("mismatch Job got: %v want: %v", test.existing, test.expected)
-			}
+			EnsureJob(modified, &existing, required)
 		})
 	}
 }
