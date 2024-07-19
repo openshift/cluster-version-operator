cb9e52345ea8429410d2768982bb00bdb5803dba
Petr Muller <muller@redhat.com>
2024-07-19 19:09:29 +0200

    Fiddle with main controlelr

--- pkg/updatestatus/updatestatuscontroller.go
+++ pkg/updatestatus/updatestatuscontroller.go
@@ -17,6 +17,11 @@ import (
 type updateStatusController struct {
 	updateStatusClient configclientv1alpha1.UpdateStatusInterface
 	recorder           events.Recorder
+
+	// updateStatus is the resource instance local to the controller, on which the USC maintains
+	// the most recently observed state. This local status is propagated to the cluster by this controller
+	// when it changes.
+	updateStatus *configv1alpha1.UpdateStatus
 }
 
 func newUpdateStatusController(updateStatusClient configclientv1alpha1.UpdateStatusInterface, eventsRecorder events.Recorder) factory.Controller {
@@ -30,17 +35,58 @@ func newUpdateStatusController(updateStatusClient configclientv1alpha1.UpdateSta
 }
 
 func (c *updateStatusController) sync(ctx context.Context, syncCtx factory.SyncContext) error {
-	klog.Info("Update Status Controller :: SYNC")
-	updateStatus, err := c.updateStatusClient.Get(ctx, "cluster", metav1.GetOptions{})
-	if apierrors.IsNotFound(err) {
+	klog.Info("USC :: SYNC")
+	var exists bool
+	updateStatus, err := c.updateStatusClient.Get(ctx, "another-cluster", metav1.GetOptions{})
+	if err != nil && !apierrors.IsNotFound(err) {
+		return err
+	}
+	exists = err == nil
+
+	if c.updateStatus == nil && exists {
+		c.updateStatus = updateStatus
+		return nil
+	}
+
+	if c.updateStatus == nil && !exists {
 		updateStatus = &configv1alpha1.UpdateStatus{
+			// TypeMeta: metav1.TypeMeta{
+			// 	Kind:       "UpdateStatus",
+			// 	APIVersion: "config.openshift.io/v1alpha1",
+			// },
 			ObjectMeta: metav1.ObjectMeta{
-				Name: "cluster",
+				Name: "another-cluster",
+			},
+			Spec: configv1alpha1.UpdateStatusSpec{},
+			Status: configv1alpha1.UpdateStatusStatus{
+				ControlPlane: configv1alpha1.ControlPlaneUpdateStatus{
+					Summary: configv1alpha1.ControlPlaneUpdateStatusSummary{
+						Assessment:           "aaaa",
+						Versions:             configv1alpha1.ControlPlaneUpdateVersions{},
+						Completion:           0,
+						StartedAt:            metav1.Time{},
+						CompletedAt:          metav1.Time{},
+						EstimatedCompletedAt: metav1.Time{},
+					},
+					Informers: []configv1alpha1.UpdateInformer{
+						{
+							Name: "YADA",
+						},
+					},
+					Conditions: nil,
+				},
+				WorkerPools: nil,
+				Conditions:  nil,
 			},
 		}
-		_, err := c.updateStatusClient.Create(ctx, updateStatus, metav1.CreateOptions{})
+		updateStatus, err := c.updateStatusClient.Create(ctx, updateStatus, metav1.CreateOptions{})
+		if err == nil {
+			c.updateStatus = updateStatus
+			syncCtx.Queue().AddRateLimited(syncCtx.QueueKey())
+		}
 		return err
 	}
 
-	return nil
+	_, err = c.updateStatusClient.UpdateStatus(ctx, c.updateStatus, metav1.UpdateOptions{})
+	return err
 }
