ae4e180b19aa4f67f2a53785813ff040ee1690da
Hongkai Liu <hongkailiu@users.noreply.github.com>
2024-09-17 10:08:20 -0400

    Improve some wording and simplify unit tests

--- pkg/cvo/sync_worker.go
+++ pkg/cvo/sync_worker.go
@@ -35,7 +35,7 @@ type ConfigSyncWorker interface {
 
 	// NotifyAboutManagedResourceActivity informs the sync worker about activity for a managed resource.
 	NotifyAboutManagedResourceActivity(msg string)
-	// Initialized returns true if ConfigSyncWorker has work to do already
+	// Initialized returns true if the worker has work to do already
 	Initialized() bool
 }
 
