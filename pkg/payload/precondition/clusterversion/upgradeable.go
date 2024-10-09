298589bddb7a6a7db0e269c8baea63901012cd96
Hongkai Liu <hongkailiu@users.noreply.github.com>
2024-10-08 21:36:21 -0400

    OCPBUGS-42880: Upgradeable=False should not block a 4.(y+1).z to 4.(y+1).z' retarget

--- pkg/payload/precondition/clusterversion/upgradeable.go
+++ pkg/payload/precondition/clusterversion/upgradeable.go
@@ -2,10 +2,9 @@ package clusterversion
 
 import (
 	"context"
-	"strconv"
-	"strings"
 	"time"
 
+	"github.com/blang/semver/v4"
 	configv1 "github.com/openshift/api/config/v1"
 	configv1listers "github.com/openshift/client-go/config/listers/config/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -13,7 +12,7 @@ import (
 	"k8s.io/klog/v2"
 
 	"github.com/openshift/cluster-version-operator/lib/resourcemerge"
-	precondition "github.com/openshift/cluster-version-operator/pkg/payload/precondition"
+	"github.com/openshift/cluster-version-operator/pkg/payload/precondition"
 )
 
 // Upgradeable checks if clusterversion is upgradeable currently.
@@ -74,30 +73,40 @@ func (pf *Upgradeable) Run(ctx context.Context, releaseContext precondition.Rele
 		return nil
 	}
 
-	// we can always allow the upgrade if there isn't a version already installed
-	if len(cv.Status.History) == 0 {
-		klog.V(2).Infof("Precondition %s passed: no release history.", pf.Name())
-		return nil
+	currentVersion, err := semver.Parse(cv.Status.Desired.Version)
+	if err != nil {
+		return &precondition.Error{
+			Nested:             err,
+			Reason:             "InvalidCurrentVersion",
+			Message:            err.Error(),
+			Name:               pf.Name(),
+			NonBlockingWarning: true, // do not block on issues that require an update to fix
+		}
 	}
 
-	currentVersion := GetCurrentVersion(cv.Status.History)
-	currentMinor := GetEffectiveMinor(currentVersion)
-	desiredMinor := GetEffectiveMinor(releaseContext.DesiredVersion)
-	klog.V(2).Infof("currentMinor %s releaseContext.DesiredVersion %s desiredMinor %s", currentMinor, releaseContext.DesiredVersion, desiredMinor)
+	targetVersion, err := semver.Parse(releaseContext.DesiredVersion)
+	if err != nil {
+		return &precondition.Error{
+			Nested:  err,
+			Reason:  "InvalidDesiredVersion",
+			Message: err.Error(),
+			Name:    pf.Name(),
+		}
+	}
 
-	// if there is no difference in the minor version (4.y.z where 4.y is the same for current and desired), then we can still upgrade
-	// if no cluster overrides have been set
-	if !minorVersionUpgrade(currentMinor, desiredMinor) {
-		klog.V(2).Infof("Precondition %q passed: minor from the target %s is not a minor version update from the current %s.%s.", pf.Name(), releaseContext.DesiredVersion, currentVersion, currentMinor)
+	klog.V(4).Infof("The current version is %s parsed from %s and the target version is %s parsed from %s", currentVersion.String(), cv.Status.Desired.Version, targetVersion.String(), releaseContext.DesiredVersion)
+	if targetVersion.LTE(currentVersion) || targetVersion.Major == currentVersion.Major && targetVersion.Minor == currentVersion.Minor {
+		// When Upgradeable==False, a patch level update with the same minor level is allowed unless overrides are set
+		// This Upgradeable precondition is only concerned about moving forward, i.e., do not care about downgrade which is taken care of by the Rollback precondition
 		if condition := ClusterVersionOverridesCondition(cv); condition != nil {
-			klog.V(2).Infof("Update from %s to %s blocked by %s: %s", currentVersion, releaseContext.DesiredVersion, condition.Reason, condition.Message)
-
+			klog.V(2).Infof("Retarget from %s to %s is blocked by %s: %s", currentVersion.String(), targetVersion.String(), condition.Reason, condition.Message)
 			return &precondition.Error{
 				Reason:  condition.Reason,
 				Message: condition.Message,
 				Name:    pf.Name(),
 			}
 		} else {
+			klog.V(2).Infof("Precondition %q passed on update to %s", pf.Name(), targetVersion.String())
 			return nil
 		}
 	}
@@ -112,45 +121,3 @@ func (pf *Upgradeable) Run(ctx context.Context, releaseContext precondition.Rele
 
 // Name returns Name for the precondition.
 func (pf *Upgradeable) Name() string { return "ClusterVersionUpgradeable" }
-
-// GetCurrentVersion determines and returns the cluster's current version by iterating through the
-// provided update history until it finds the first version with update State of Completed. If a
-// Completed version is not found the version of the oldest history entry, which is the originally
-// installed version, is returned. If history is empty the empty string is returned.
-func GetCurrentVersion(history []configv1.UpdateHistory) string {
-	for _, h := range history {
-		if h.State == configv1.CompletedUpdate {
-			klog.V(2).Infof("Cluster current version=%s", h.Version)
-			return h.Version
-		}
-	}
-	// Empty history should only occur if method is called early in startup before history is populated.
-	if len(history) != 0 {
-		return history[len(history)-1].Version
-	}
-	return ""
-}
-
-// GetEffectiveMinor attempts to do a simple parse of the version provided.  If it does not parse, the value is considered
-// empty string, which works for the comparison done here for equivalence.
-func GetEffectiveMinor(version string) string {
-	splits := strings.Split(version, ".")
-	if len(splits) < 2 {
-		return ""
-	}
-	return splits[1]
-}
-
-// minorVersionUpgrade returns true if the the desired update minor version number is greater
-// than the current version minor version number. Errors resulting from either version
-// number being unset or NaN are ignored simply resulting in false returned.
-func minorVersionUpgrade(currentMinor string, desiredMinor string) bool {
-	if currentMinorNum, err := strconv.Atoi(currentMinor); err == nil {
-		if desiredMinorNum, err := strconv.Atoi(desiredMinor); err == nil {
-			if desiredMinorNum > currentMinorNum {
-				return true
-			}
-		}
-	}
-	return false
-}
