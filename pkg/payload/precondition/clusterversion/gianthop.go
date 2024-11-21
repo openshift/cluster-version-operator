6c0eaed6e066f74e9fc5eeace37ced32fa1fac55
W. Trevor King <wking@tremily.us>
2024-11-21 10:21:20 -0800

    pkg/payload/precondition/clusterversion: New GiantHop update precondition
    From our docs [1]:
    
      Due to fundamental Kubernetes design, all OpenShift Container
      Platform updates between minor versions must be serialized. You must
      update from OpenShift Container Platform <4.y> to <4.y+1>, and then
      to <4.y+2>. You cannot update from OpenShift Container Platform
      <4.y> to <4.y+2> directly. However, administrators who want to
      update between two even-numbered minor versions can do so incurring
      only a single reboot of non-control plane hosts.
    
    This commit adds a new precondition that enforces that policy, so
    cluster admins who run '--to-image ...' don't hop straight from 4.y.z
    to 4.(y+2).z' or similar without realizing that they were outpacing
    testing and policy.
    
    [1]: https://docs.openshift.com/container-platform/4.17/updating/updating_a_cluster/control-plane-only-update.html

---
+++ pkg/payload/precondition/clusterversion/gianthop.go
@@ -0,0 +1,87 @@
+package clusterversion
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/blang/semver/v4"
+	configv1listers "github.com/openshift/client-go/config/listers/config/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+
+	"github.com/openshift/cluster-version-operator/pkg/payload/precondition"
+)
+
+// GiantHop blocks giant hops from the version that is currently being reconciled.
+type GiantHop struct {
+	key    string
+	lister configv1listers.ClusterVersionLister
+}
+
+// NewGiantHop returns a new GiantHop precondition check.
+func NewGiantHop(lister configv1listers.ClusterVersionLister) *GiantHop {
+	return &GiantHop{
+		key:    "version",
+		lister: lister,
+	}
+}
+
+// Name returns Name for the precondition.
+func (p *GiantHop) Name() string { return "ClusterVersionGiantHop" }
+
+// Run runs the GiantHop precondition, blocking giant hops from the
+// version that is currently being reconciled.  It returns a
+// PreconditionError when possible.
+func (p *GiantHop) Run(ctx context.Context, releaseContext precondition.ReleaseContext) error {
+	cv, err := p.lister.Get(p.key)
+	if apierrors.IsNotFound(err) || meta.IsNoMatchError(err) {
+		return nil
+	}
+	if err != nil {
+		return &precondition.Error{
+			Nested:  err,
+			Reason:  "UnknownError",
+			Message: err.Error(),
+			Name:    p.Name(),
+		}
+	}
+
+	currentVersion, err := semver.Parse(cv.Status.Desired.Version)
+	if err != nil {
+		return &precondition.Error{
+			Nested:             err,
+			Reason:             "InvalidCurrentVersion",
+			Message:            err.Error(),
+			Name:               p.Name(),
+			NonBlockingWarning: true, // do not block on issues that require an update to fix
+		}
+	}
+
+	targetVersion, err := semver.Parse(releaseContext.DesiredVersion)
+	if err != nil {
+		return &precondition.Error{
+			Nested:  err,
+			Reason:  "InvalidDesiredVersion",
+			Message: err.Error(),
+			Name:    p.Name(),
+		}
+	}
+
+	if targetVersion.Major > currentVersion.Major {
+		return &precondition.Error{
+			Reason:  "MajorVersionUpdate",
+			Message: fmt.Sprintf("%s has a larger major version than the current target %s (%d > %d), and only updates within the current major version are supported.", targetVersion, currentVersion, targetVersion.Major, currentVersion.Major),
+			Name:    p.Name(),
+		}
+	}
+
+	if targetVersion.Minor > currentVersion.Minor+1 {
+		return &precondition.Error{
+			Reason:  "MultipleMinorVersionsUpdate",
+			Message: fmt.Sprintf("%s is more than one minor version beyond the current target %s (%d.%d > %d.(%d+1)), and only updates within the current minor version or to the next minor version are supported.", targetVersion, currentVersion, targetVersion.Major, targetVersion.Minor, currentVersion.Major, currentVersion.Minor),
+			Name:    p.Name(),
+		}
+	}
+
+	return nil
+}
