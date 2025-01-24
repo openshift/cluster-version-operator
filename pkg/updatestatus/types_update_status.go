8a55ae2d44dd28cd041a62bff76b586c4d42fb16
Petr Muller <muller@redhat.com>
2025-01-24 18:15:13 +0100

    USC: Pull latest UpdateStatus API code
    The `UpdateStatus` API PR was recently updated to a version proposed for review & potential approval and merge.
    
    https://github.com/openshift/api/pull/2012/commits/4758efa2af8f977edcb1e1753931b04176fbdaf0

--- pkg/updatestatus/types_update_status.go
+++ pkg/updatestatus/types_update_status.go
@@ -12,20 +12,23 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-// +kubebuilder:resource:path=updatestatuses,scope=Namespaced
+// +kubebuilder:resource:path=updatestatuses,scope=Cluster
 // +openshift:api-approved.openshift.io=https://github.com/openshift/api/pull/2012
 // +openshift:file-pattern=cvoRunLevel=0000_00,operatorName=cluster-version-operator,operatorOrdering=02
 // +openshift:enable:FeatureGate=UpgradeStatus
 // +kubebuilder:metadata:annotations="description=Provides health and status information about OpenShift cluster updates."
 // +kubebuilder:metadata:annotations="displayName=UpdateStatuses"
 type UpdateStatus struct {
-	metav1.TypeMeta   `json:",inline"`
+	metav1.TypeMeta `json:",inline"`
+
+	// +optional
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	// spec is empty for now, UpdateStatus is purely status-reporting API. In the future spec may be used to hold
 	// configuration to drive what information is surfaced and how
-	// +kubebuilder:validation:Required
+	// +required
 	Spec UpdateStatusSpec `json:"spec"`
+	// status exposes the health and status of the ongoing cluster update
 	// +optional
 	Status UpdateStatusStatus `json:"status"`
 }
@@ -40,25 +43,40 @@ type UpdateStatusSpec struct {
 // UpdateStatusStatus is the API about in-progress updates. It aggregates and summarizes UpdateInsights produced by
 // update informers
 type UpdateStatusStatus struct {
+	// conditions provide details about the controller operational matters
+	// +listType=map
+	// +listMapKey=type
+	// +patchStrategy=merge
+	// +patchMergeKey=type
+	// +optional
+	// +kubebuilder:validation:MaxItems=10
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+
 	// controlPlane contains a summary and insights related to the control plane update
-	// +kubebuilder:validation:Required
+	// +required
 	ControlPlane ControlPlane `json:"controlPlane"`
 
 	// workerPools contains summaries and insights related to the worker pools update
 	// +listType=map
 	// +listMapKey=name
+	// +patchStrategy=merge
+	// +patchMergeKey=name
 	// +optional
-	WorkerPools []Pool `json:"workerPools,omitempty"`
-
-	// conditions provide details about the controller operational matters
-	// +listType=map
-	// +listMapKey=type
-	// +optional
-	Conditions []metav1.Condition `json:"conditions,omitempty"`
+	// +kubebuilder:validation:MaxItems=32
+	WorkerPools []Pool `json:"workerPools,omitempty" patchStrategy:"merge" patchMergeKey:"name"`
 }
 
 // ControlPlane contains a summary and insights related to the control plane update
 type ControlPlane struct {
+	// conditions provides details about the control plane update
+	// +listType=map
+	// +listMapKey=type
+	// +patchStrategy=merge
+	// +patchMergeKey=type
+	// +optional
+	// +kubebuilder:validation:MaxItems=10
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+
 	// resource is the resource that represents the control plane. It will typically be a ClusterVersion resource
 	// in standalone OpenShift and HostedCluster in Hosted Control Planes.
 	//
@@ -66,7 +84,7 @@ type ControlPlane struct {
 	// only the "correct" resource types to be referenced (here, ClusterVersion and HostedCluster). However, because we
 	// use resource references in many places and this API is intended to be consumed by clients, not produced, consistency
 	// seems to be more valuable than type safety for producers.
-	// +kubebuilder:validation:Required
+	// +required
 	Resource ResourceRef `json:"resource"`
 
 	// poolResource is the resource that represents control plane node pool, typically a MachineConfigPool. This field
@@ -82,14 +100,11 @@ type ControlPlane struct {
 	// informers is a list of insight producers, each carries a list of insights relevant for control plane
 	// +listType=map
 	// +listMapKey=name
+	// +patchStrategy=merge
+	// +patchMergeKey=name
 	// +optional
-	Informers []Informer `json:"informers,omitempty"`
-
-	// conditions provides details about the control plane update
-	// +listType=map
-	// +listMapKey=type
-	// +optional
-	Conditions []metav1.Condition `json:"conditions,omitempty"`
+	// +kubebuilder:validation:MaxItems=16
+	Informers []ControlPlaneInformer `json:"informers,omitempty" patchStrategy:"merge" patchMergeKey:"name"`
 }
 
 // ControlPlaneConditionType are types of conditions that can be reported on control plane level
@@ -107,20 +122,33 @@ type ControlPlaneUpdatingReason string
 const (
 	// ClusterVersionProgressing is used for Updating=True set because we observed a ClusterVersion resource to
 	// have Progressing=True condition
-	ReasonClusterVersionProgressing ControlPlaneUpdatingReason = "ClusterVersionProgressing"
+	ControlPlaneClusterVersionProgressing ControlPlaneUpdatingReason = "ClusterVersionProgressing"
 	// ClusterVersionNotProgressing is used for Updating=False set because we observed a ClusterVersion resource to
 	// have Progressing=False condition
-	ReasonClusterVersionNotProgressing ControlPlaneUpdatingReason = "ClusterVersionNotProgressing"
+	ControlPlaneClusterVersionNotProgressing ControlPlaneUpdatingReason = "ClusterVersionNotProgressing"
 	// CannotDetermineUpdating is used with Updating=Unknown. This covers many different actual reasons such as
 	// missing or Unknown Progressing condition on ClusterVersion, but it does not seem useful to track the individual
 	// reasons to that granularity for Updating=Unknown
-	ReasonClusterVersionCannotDetermine ControlPlaneUpdatingReason = "CannotDetermineUpdating"
+	ControlPlaneCannotDetermineUpdating ControlPlaneUpdatingReason = "CannotDetermineUpdating"
 )
 
 // Pool contains a summary and insights related to a node pool update
 type Pool struct {
+	// conditions provide details about the pool
+	// +listType=map
+	// +listMapKey=type
+	// +patchStrategy=merge
+	// +patchMergeKey=type
+	// +optional
+	// +kubebuilder:validation:MaxItems=10
+	Conditions []metav1.Condition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+
 	// name is the name of the pool
-	// +kubebuilder:validation:Required
+	// +required
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=63
+	// +kubebuilder:validation:Pattern=`^[a-z0-9]([-a-z0-9]*[a-z0-9])?$`
 	Name string `json:"name"`
 
 	// resource is the resource that represents the pool
@@ -129,56 +157,103 @@ type Pool struct {
 	// only the "correct" resource types to be referenced (here, MachineConfigPool or NodePool). However, because we use
 	// resource references in many places and this API is intended to be consumed by clients, not produced, consistency
 	// seems to be more valuable than type safety for producers.
-	// +kubebuilder:validation:Required
+	// +required
 	Resource PoolResourceRef `json:"resource"`
 
 	// informers is a list of insight producers, each carries a list of insights
 	// +listType=map
 	// +listMapKey=name
+	// +patchStrategy=merge
+	// +patchMergeKey=name
 	// +optional
-	Informers []Informer `json:"informers,omitempty"`
-
-	// conditions provide details about the pool
-	// +listType=map
-	// +listMapKey=type
-	// +optional
-	Conditions []metav1.Condition `json:"conditions,omitempty"`
+	// +kubebuilder:validation:MaxItems=16
+	Informers []WorkerPoolInformer `json:"informers,omitempty" patchStrategy:"merge" patchMergeKey:"name"`
 }
 
-// Informer is an insight producer identified by a name, carrying a list of insights it produced
-type Informer struct {
+// ControlPlaneInformer is an insight producer identified by a name, carrying a list of insights it produced
+type ControlPlaneInformer struct {
 	// name is the name of the insight producer
-	// +kubebuilder:validation:Required
+	// +required
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=63
+	// +kubebuilder:validation:Pattern=`^[a-z0-9]([-a-z0-9]*[a-z0-9])?$`
 	Name string `json:"name"`
 
 	// insights is a list of insights produced by this producer
-	// +optional
 	// +listType=map
 	// +listMapKey=uid
-	Insights []Insight `json:"insights,omitempty"`
+	// +patchStrategy=merge
+	// +patchMergeKey=uid
+	// +optional
+	// +kubebuilder:validation:MaxItems=128
+	Insights []ControlPlaneInsight `json:"insights,omitempty" patchStrategy:"merge" patchMergeKey:"uid"`
 }
 
-// Insight is a unique piece of either status/progress or update health information produced by update informer
-type Insight struct {
-	// uid identifies the insight over time
-	// +kubebuilder:validation:Required
+// WorkerPoolInformer is an insight producer identified by a name, carrying a list of insights it produced
+type WorkerPoolInformer struct {
+	// name is the name of the insight producer
+	// +required
 	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=63
+	// +kubebuilder:validation:Pattern=`^[a-z0-9]([-a-z0-9]*[a-z0-9])?$`
+	Name string `json:"name"`
+
+	// insights is a list of insights produced by this producer
+	// +listType=map
+	// +listMapKey=uid
+	// +patchStrategy=merge
+	// +patchMergeKey=uid
+	// +optional
+	// +kubebuilder:validation:MaxItems=1024
+	Insights []WorkerPoolInsight `json:"insights,omitempty" patchStrategy:"merge" patchMergeKey:"uid"`
+}
+
+// ControlPlaneInsight is a unique piece of either status/progress or update health information produced by update informer
+type ControlPlaneInsight struct {
+	// uid identifies the insight over time
+	// +required
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=63
+	// +kubebuilder:validation:Pattern=`^[a-z0-9]([-a-z0-9]*[a-z0-9])?$`
 	UID string `json:"uid"`
 
 	// acquiredAt is the time when the data was acquired by the producer
-	// +kubebuilder:validation:Required
+	// +required
 	// +kubebuilder:validation:Type=string
 	// +kubebuilder:validation:Format=date-time
 	AcquiredAt metav1.Time `json:"acquiredAt"`
 
-	InsightUnion `json:",inline"`
+	ControlPlaneInsightUnion `json:",inline"`
 }
 
-// InsightUnion is the discriminated union of all insights types, identified by type field
-type InsightUnion struct {
+// WorkerPoolInsight is a unique piece of either status/progress or update health information produced by update informer
+type WorkerPoolInsight struct {
+	// uid identifies the insight over time
+	// +required
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=63
+	// +kubebuilder:validation:Pattern=`^[a-z0-9]([-a-z0-9]*[a-z0-9])?$`
+	UID string `json:"uid"`
+
+	// acquiredAt is the time when the data was acquired by the producer
+	// +required
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:Format=date-time
+	AcquiredAt metav1.Time `json:"acquiredAt"`
+
+	WorkerPoolInsightUnion `json:",inline"`
+}
+
+// ControlPlaneInsightUnion is the discriminated union of all insights types that can be reported for the control plane,
+// identified by type field
+type ControlPlaneInsightUnion struct {
 	// type identifies the type of the update insight
 	// +unionDiscriminator
-	// +kubebuilder:validation:Required
+	// +required
 	// +kubebuilder:validation:Enum=ClusterVersion;ClusterOperator;MachineConfigPool;Node;Health
 	Type InsightType `json:"type"`
 
@@ -213,9 +288,36 @@ type InsightUnion struct {
 	HealthInsight *HealthInsight `json:"health,omitempty"`
 }
 
+// WorkerPoolInsightUnion is the discriminated union of insights types that can be reported for a worker pool,
+// identified by type field
+type WorkerPoolInsightUnion struct {
+	// type identifies the type of the update insight
+	// +unionDiscriminator
+	// +required
+	// +kubebuilder:validation:Enum=MachineConfigPool;Node;Health
+	Type InsightType `json:"type"`
+
+	// machineConfigPool is a status insight about the state of a worker pool update, where the worker pool
+	// is represented by a MachineConfigPool resource
+	// +optional
+	// +unionMember
+	MachineConfigPoolStatusInsight *MachineConfigPoolStatusInsight `json:"machineConfigPool,omitempty"`
+
+	// node is a status insight about the state of a worker node update, where the worker node is represented
+	// by a Node resource
+	// +optional
+	// +unionMember
+	NodeStatusInsight *NodeStatusInsight `json:"node,omitempty"`
+
+	// health is a generic health insight about the update. It does not represent a status of any specific
+	// resource but surfaces actionable information about the health of the cluster or an update
+	// +optional
+	// +unionMember
+	HealthInsight *HealthInsight `json:"health,omitempty"`
+}
+
 // InsightType identifies the type of the update insight as either one of the resource-specific status insight,
 // or a generic health insight
-// +kubebuilder:validation:Enum=ClusterVersion;ClusterOperator;MachineConfigPool;Node;Health
 type InsightType string
 
 const (
@@ -243,18 +345,29 @@ const (
 type ResourceRef struct {
 	// group of the object being referenced, if any
 	// +optional
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:MaxLength=253
 	Group string `json:"group,omitempty"`
 
 	// resource of object being referenced
-	// +kubebuilder:validation:Required
+	// +required
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:MaxLength=253
+	// +kubebuilder:validation:MinLength=1
 	Resource string `json:"resource"`
 
 	// name of the object being referenced
-	// +kubebuilder:validation:Required
+	// +required
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:MaxLength=253
+	// +kubebuilder:validation:MinLength=1
 	Name string `json:"name"`
 
 	// namespace of the object being referenced, if any
 	// +optional
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:MaxLength=253
+	// +kubebuilder:validation:Pattern=`^[a-z0-9]([-a-z0-9]*[a-z0-9])?$`
 	Namespace string `json:"namespace,omitempty"`
 }
 
@@ -271,7 +384,11 @@ type PoolResourceRef struct {
 // +openshift:compatibility-gen:level=4
 type UpdateStatusList struct {
 	metav1.TypeMeta `json:",inline"`
+	// +optional
 	metav1.ListMeta `json:"metadata"`
 
+	// items is a  list of UpdateStatus resources
+	// +optional
+	// +kubebuilder:validation:MaxItems=32
 	Items []UpdateStatus `json:"items"`
 }
