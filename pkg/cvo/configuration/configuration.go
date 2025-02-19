9ce6a03c1c2d6e4d0baf01b5c20d079fb1900b26
David Hurta <dhurta@redhat.com>
2025-02-19 19:03:02 +0100

    pkg: Add CVOConfiguration controller
    The controller has an empty reconcile logic as of now.
    The logic will be implemented later. The goal of this commit is to introduce
    a new CVO controller, which can optionally (depending on the state of the
    CVOConfiguration feature gate) create a new informer.
    
    The purpose of the `Start` method is to make the creation of a ClusterVersionOperator informer
    optional, and to make the creation possible later in the CVO logic by restarting the operator
    informer factory. To decide whether to create the informer, feature gates must be known,
    which happens normally later in the run when all the other informer factories are already
    created and inaccessible to the main CVO controller.
    
    Related enhancement: [1]
    
    [1]: https://github.com/openshift/enhancements/blob/2890cccf20ebcb94fce901f7afb170ca680aa2d9/enhancements/update/cvo-log-level-api.md

---
+++ pkg/cvo/configuration/configuration.go
@@ -0,0 +1,111 @@
+package configuration
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
+
+	operatorv1alpha1 "github.com/openshift/api/operator/v1alpha1"
+	operatorclientset "github.com/openshift/client-go/operator/clientset/versioned"
+	cvoclientv1alpha1 "github.com/openshift/client-go/operator/clientset/versioned/typed/operator/v1alpha1"
+	operatorexternalversions "github.com/openshift/client-go/operator/informers/externalversions"
+	operatorlistersv1alpha1 "github.com/openshift/client-go/operator/listers/operator/v1alpha1"
+)
+
+const ClusterVersionOperatorConfigurationName = "cluster"
+
+type ClusterVersionOperatorConfiguration struct {
+	queueKey string
+	// queue tracks checking for the CVO configuration.
+	//
+	// The type any is used to comply with the worker method of the cvo.Operator struct.
+	queue workqueue.TypedRateLimitingInterface[any]
+
+	client  cvoclientv1alpha1.ClusterVersionOperatorInterface
+	lister  operatorlistersv1alpha1.ClusterVersionOperatorLister
+	factory operatorexternalversions.SharedInformerFactory
+
+	started bool
+}
+
+func (config *ClusterVersionOperatorConfiguration) Queue() workqueue.TypedRateLimitingInterface[any] {
+	return config.queue
+}
+
+// clusterVersionOperatorEventHandler queues an update for the cluster version operator on any change to the given object.
+// Callers should use this with an informer.
+func (config *ClusterVersionOperatorConfiguration) clusterVersionOperatorEventHandler() cache.ResourceEventHandler {
+	return cache.ResourceEventHandlerFuncs{
+		AddFunc: func(_ interface{}) {
+			config.queue.Add(config.queueKey)
+		},
+		UpdateFunc: func(_, _ interface{}) {
+			config.queue.Add(config.queueKey)
+		},
+		DeleteFunc: func(_ interface{}) {},
+	}
+}
+
+// NewClusterVersionOperatorConfiguration returns ClusterVersionOperatorConfiguration, which might be used
+// to synchronize with the ClusterVersionOperator resource.
+func NewClusterVersionOperatorConfiguration(client operatorclientset.Interface, factory operatorexternalversions.SharedInformerFactory) *ClusterVersionOperatorConfiguration {
+	return &ClusterVersionOperatorConfiguration{
+		queueKey: fmt.Sprintf("ClusterVersionOperator/%s", ClusterVersionOperatorConfigurationName),
+		queue: workqueue.NewTypedRateLimitingQueueWithConfig[any](
+			workqueue.DefaultTypedControllerRateLimiter[any](),
+			workqueue.TypedRateLimitingQueueConfig[any]{Name: "configuration"}),
+		client:  client.OperatorV1alpha1().ClusterVersionOperators(),
+		factory: factory,
+	}
+}
+
+// Start initializes and starts the configuration's informers. Must be run before Sync is called.
+// Blocks until informers caches are synchronized or the context is cancelled.
+func (config *ClusterVersionOperatorConfiguration) Start(ctx context.Context) error {
+	informer := config.factory.Operator().V1alpha1().ClusterVersionOperators()
+	if _, err := informer.Informer().AddEventHandler(config.clusterVersionOperatorEventHandler()); err != nil {
+		return err
+	}
+	config.lister = informer.Lister()
+
+	config.factory.Start(ctx.Done())
+	synced := config.factory.WaitForCacheSync(ctx.Done())
+	for _, ok := range synced {
+		if !ok {
+			return fmt.Errorf("caches failed to sync: %w", ctx.Err())
+		}
+	}
+
+	config.started = true
+	return nil
+}
+
+func (config *ClusterVersionOperatorConfiguration) Sync(ctx context.Context, key string) error {
+	if !config.started {
+		panic("ClusterVersionOperatorConfiguration instance was not properly started before its synchronization.")
+	}
+	startTime := time.Now()
+	klog.V(2).Infof("Started syncing CVO configuration %q", key)
+	defer func() {
+		klog.V(2).Infof("Finished syncing CVO configuration (%v)", time.Since(startTime))
+	}()
+
+	desiredConfig, err := config.lister.Get(ClusterVersionOperatorConfigurationName)
+	if apierrors.IsNotFound(err) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	return config.sync(ctx, desiredConfig)
+}
+
+func (config *ClusterVersionOperatorConfiguration) sync(_ context.Context, _ *operatorv1alpha1.ClusterVersionOperator) error {
+	klog.Infof("ClusterVersionOperator configuration has been synced")
+	return nil
+}
