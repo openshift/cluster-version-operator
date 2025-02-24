c67e5040387f2273d543b2b0b2bbf477acd389c7
David Hurta <dhurta@redhat.com>
2025-02-24 14:23:07 +0100

    pkg/internal: Add log level constants
    These constants can be simply used when logging across the project instead
    of specifying the level by a number. The descriptions and the values will
    also guide developers when deciding what log level to choose.
    
    For example:
    
    ```go
      import "github.com/openshift/cluster-version-operator/pkg/internal"
    
      klog.V(internal.Debug).Infof("")
    ```
    
    The descriptions of the values are extracted from the used API type [1].
    
    [1]: https://github.com/openshift/api/blob/744790f2cff777b1bb29bdccce10e4e84cff0a69/operator/v1/types.go#L94-L109

--- pkg/internal/constants.go
+++ pkg/internal/constants.go
@@ -1,5 +1,7 @@
 package internal
 
+import "k8s.io/klog/v2"
+
 const (
 	ConfigNamespace        = "openshift-config"
 	ConfigManagedNamespace = "openshift-config-managed"
@@ -7,4 +9,23 @@ const (
 	AdminAcksConfigMap     = "admin-acks"
 	InstallerConfigMap     = "openshift-install"
 	ManifestsConfigMap     = "openshift-install-manifests"
+)
+
+var (
+	// Normal is the default. Normal, working log information, everything is fine, but helpful notices for auditing or
+	// common operations. In kube, this is probably glog=2.
+	Normal klog.Level = 2
+
+	// Debug is used when something went wrong. Even common operations may be logged, and less helpful but more
+	// quantity of notices. In kube, this is probably glog=4.
+	Debug klog.Level = 4
+
+	// Trace is used when something went really badly and even more verbose logs are needed. Logging every function
+	// call as part of a common operation, to tracing execution of a query. In kube, this is probably glog=6.
+	Trace klog.Level = 6
+
+	// TraceAll is used when something is broken at the level of API content/decoding. It will dump complete body
+	// content. If you turn this on in a production cluster prepare from serious performance issues and massive
+	// amounts of logs. In kube, this is probably glog=8.
+	TraceAll klog.Level = 8
 )
