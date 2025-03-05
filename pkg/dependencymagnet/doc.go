0153756a1edc75f80b641417b9891fc2f7756e43
Petr Muller <muller@redhat.com>
2025-03-05 14:25:52 +0100

    USC: Import UpdateStatus CRD from o/api

--- pkg/dependencymagnet/doc.go
+++ pkg/dependencymagnet/doc.go
@@ -6,4 +6,5 @@ package dependencymagnet
 import (
 	_ "github.com/openshift/api/config/v1/zz_generated.crd-manifests"
 	_ "github.com/openshift/api/operator/v1alpha1/zz_generated.crd-manifests"
+	_ "github.com/openshift/api/update/v1alpha1/zz_generated.crd-manifests"
 )
