289e9762adac33d4833da8fd436f7f2906f26699
David Hurta <dhurta@redhat.com>
2025-05-06 16:35:00 +0200

    hack: Extract build variables to separate file
    To reuse the logic across multiple files.

--- hack/build-go.sh
+++ hack/build-go.sh
@@ -2,29 +2,11 @@
 
 set -eu
 
-REPO=github.com/openshift/cluster-version-operator
-GOFLAGS=${GOFLAGS:--mod=vendor}
-GLDFLAGS=${GLDFLAGS:-}
-
-eval $(go env | grep -e "GOHOSTOS" -e "GOHOSTARCH")
-
-GOOS=${GOOS:-${GOHOSTOS}}
-GOARCH=${GOARCH:-${GOHOSTARCH}}
-
 # Go to the root of the repo
 cd "$(git rev-parse --show-cdup)"
 
-VERSION_OVERRIDE=${VERSION_OVERRIDE:-${OS_GIT_VERSION:-}}
-if [ -z "${VERSION_OVERRIDE:-}" ]; then
-	echo "Using version from git..."
-	VERSION_OVERRIDE=$(git describe --abbrev=8 --dirty --always)
-fi
-
-eval $(go env)
-
-if [ -z ${BIN_PATH+a} ]; then
-	export BIN_PATH=_output/${GOOS}/${GOARCH}
-fi
+# Source build variables
+source hack/build-info.sh
 
 echo "Building binaries into ${BIN_PATH}"
 mkdir -p ${BIN_PATH}
