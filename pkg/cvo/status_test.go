c2c083152e84f238f04fbe5218b418ada5f9da4c
Hongkai Liu <hongkailiu@users.noreply.github.com>
2025-05-27 14:02:58 -0400

    Clean up the condition with type=ImplicitlyEnabled
    This commit addresses the issue found in the last round of pre-merge
    test [1].
    
    This commit could be reverted after maximal 2 releases.
    (It is alright to keep it too)
    Assume this PR gets merged to main which is ATM dev branch: 4.20.
    When the main branches points to 4.22, all the clusters affetecd
    by the bug (originally 4.19 and 4.20) should be fixed when they
    are upgraded to 4.20/4.21. Thus any 4.22 cluster should not be
    affected.
    
    [1]. https://issues.redhat.com/browse/OCPBUGS-56114?focusedId=27251461&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel#comment-27251461

--- pkg/cvo/status_test.go
+++ pkg/cvo/status_test.go
@@ -871,7 +871,11 @@ func TestUpdateClusterVersionStatus_FilteringMultipleErrorsForFailingCondition(t
 			}
 			for _, c := range combinations {
 				tc.args.syncWorkerStatus.Reconciling = c.isReconciling
-				cvStatus := &configv1.ClusterVersionStatus{}
+				cvStatus := &configv1.ClusterVersionStatus{
+					Conditions: []configv1.ClusterOperatorStatusCondition{
+						{Type: "ImplicitlyEnabled", Message: "to be removed"},
+					},
+				}
 				if !c.isHistoryEmpty {
 					cvStatus.History = []configv1.UpdateHistory{{State: configv1.PartialUpdate}}
 				}
@@ -890,6 +894,10 @@ func TestUpdateClusterVersionStatus_FilteringMultipleErrorsForFailingCondition(t
 					if diff := cmp.Diff(tc.expectedProgressingCondition, progressingCondition, ignoreLastTransitionTime); diff != "" {
 						t.Errorf("unexpected progressingCondition when Reconciling == %t && isHistoryEmpty == %t\n:%s", c.isReconciling, c.isHistoryEmpty, diff)
 					}
+				}
+				conditionRemoved := resourcemerge.FindOperatorStatusCondition(cvStatus.Conditions, "ImplicitlyEnabled")
+				if conditionRemoved != nil {
+					t.Errorf("ImplicitlyEnabled condition should be removed but is still there when Reconciling == %t && isHistoryEmpty == %t", c.isReconciling, c.isHistoryEmpty)
 				}
 			}
 			payload.COUpdateStartTimesRemove("machine-config")
