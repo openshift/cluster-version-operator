626cb1610079a3a2de5e09f2948d02139d5122be
Hongkai Liu <hongkailiu@users.noreply.github.com>
2025-11-04 11:26:08 -0500

    NO-JIRA: Clarify test names in OTE
    This pull add a section to clarify test names in OTE.
    It modifies `hack/update-test-metadata.sh` to give a
    hint of what to do to fix the issue.

--- hack/update-test-metadata.sh
+++ hack/update-test-metadata.sh
@@ -5,4 +5,8 @@ set -eu
 source hack/build-info.sh
 
 # Update test metadata
-eval "${BIN_PATH}/cluster-version-operator-tests" "update"
+if ! "${BIN_PATH}/cluster-version-operator-tests" "update"; then
+  >&2 echo "Failed to update metadata. It is likely because an incompatible change is introduced, such as a test is renamed.
+See https://github.com/openshift/cluster-version-operator/blob/main/cmd/cluster-version-operator-tests/README.md#test-names for details."
+  exit 1
+fi
