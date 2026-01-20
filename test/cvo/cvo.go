b3debe7cc77cbb72ed19b649717dbecbceb4ca78
jiajliu <jiajliu@redhat.com>
2026-01-20 10:22:25 +0800

    Reapply "OTA-1604: migrate ocp-46922 from otp to cvo repo"
    This reverts commit 278da7be950c74e1856b8165b8b404755bdd974e.

--- test/cvo/cvo.go
+++ test/cvo/cvo.go
@@ -1,6 +1,12 @@
 package cvo
 
 import (
+	"context"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
 	g "github.com/onsi/ginkgo/v2"
 	o "github.com/onsi/gomega"
 
@@ -9,6 +15,8 @@ import (
 )
 
 var logger = g.GinkgoLogr.WithName("cluster-version-operator-tests")
+
+const cvoNamespace = "openshift-cluster-version"
 
 var _ = g.Describe(`[Jira:"Cluster Version Operator"] cluster-version-operator-tests`, func() {
 	g.It("should support passing tests", func() {
@@ -23,5 +31,50 @@ var _ = g.Describe(`[Jira:"Cluster Version Operator"] cluster-version-operator-t
 		output, err := ocClient.Version(ocapi.VersionOptions{Client: true})
 		o.Expect(err).NotTo(o.HaveOccurred())
 		o.Expect(output).To(o.ContainSubstring("Client Version:"))
+	})
+})
+
+// CVO tests which need access the live cluster will be placed here
+var _ = g.Describe(`[Jira:"Cluster Version Operator"] cluster-version-operator`, func() {
+	var (
+		restCfg    *rest.Config
+		kubeClient kubernetes.Interface
+	)
+
+	g.BeforeEach(func() {
+		var err error
+		// Respects KUBECONFIG env var
+		restCfg, err = GetRestConfig()
+		o.Expect(err).NotTo(o.HaveOccurred(), "Failed to load Kubernetes configuration. Please ensure KUBECONFIG environment variable is set.")
+
+		kubeClient, err = GetKubeClient(restCfg)
+		o.Expect(err).NotTo(o.HaveOccurred(), "Failed to create Kubernetes client")
+	})
+
+	// Migrated from case NonHyperShiftHOST-Author:jiajliu-Low-46922-check runlevel and scc in cvo ns
+	// Refer to https://github.com/openshift/openshift-tests-private/blob/40374cf20946ff03c88712839a5626af2c88ab31/test/extended/ota/cvo/cvo.go#L1081
+	g.It("should have correct runlevel and scc", func() {
+		ctx := context.Background()
+		err := SkipIfHypershift(ctx, restCfg)
+		o.Expect(err).NotTo(o.HaveOccurred(), "Failed to determine if cluster is HyperShift")
+
+		g.By("Checking that the 'openshift.io/run-level' label exists on the namespace and has the empty value")
+		ns, err := kubeClient.CoreV1().Namespaces().Get(ctx, cvoNamespace, metav1.GetOptions{})
+		o.Expect(err).NotTo(o.HaveOccurred(), "Failed to get namespace %s", cvoNamespace)
+		runLevel, exists := ns.ObjectMeta.Labels["openshift.io/run-level"]
+		o.Expect(exists).To(o.BeTrue(), "The 'openshift.io/run-level' label on namespace %s does not exist", cvoNamespace)
+		o.Expect(runLevel).To(o.BeEmpty(), "Expected the 'openshift.io/run-level' label value on namespace %s has the empty value, but got %s", cvoNamespace, runLevel)
+
+		g.By("Checking that the annotation 'openshift.io/scc annotation' on the CVO pod has the value hostaccess")
+		podList, err := kubeClient.CoreV1().Pods(cvoNamespace).List(ctx, metav1.ListOptions{
+			LabelSelector: "k8s-app=cluster-version-operator",
+			FieldSelector: "status.phase=Running",
+		})
+		o.Expect(err).NotTo(o.HaveOccurred(), "Failed to list running CVO pods")
+		o.Expect(podList.Items).To(o.HaveLen(1), "Expected exactly one running CVO pod, but found: %d", len(podList.Items))
+
+		cvoPod := podList.Items[0]
+		sccAnnotation := cvoPod.ObjectMeta.Annotations["openshift.io/scc"]
+		o.Expect(sccAnnotation).To(o.Equal("hostaccess"), "Expected the annotation 'openshift.io/scc annotation' on pod %s to have the value 'hostaccess', but got %s", cvoPod.Name, sccAnnotation)
 	})
 })
