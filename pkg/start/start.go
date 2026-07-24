182246834cac8390cd39b298db1e60a5c3e08dd0
W. Trevor King <wking@tremily.us>
2026-07-23 21:13:27 -0700

    pkg/start: Add apiextension to the Runtime client scheme
    Avoid:
    
      $ oc -n openshift-cluster-version logs -l k8s-app=cluster-version-operator --tail -1 | grep controller.go | tail -n4
      I0724 03:17:24.334404       1 controller.go:186] Started syncing CVO configuration "ClusterVersionOperator/agenticrun-lifecycle-controller"
      I0724 03:17:24.334427       1 controller.go:163] AgenticRun CustomResourceDefinition available? false (no kind is registered for the type v1.CustomResourceDefinition in scheme "k8s.io/client-go/kubernetes/scheme/register.go:84")
      I0724 03:17:24.334445       1 controller.go:193] Failed to disable console plugin, skipping manifest cleanup: getting console operator config: no kind is registered for the type v1.Console in scheme "k8s.io/client-go/kubernetes/scheme/register.go:84"
      I0724 03:17:24.334449       1 controller.go:188] Finished syncing CVO configuration (47.291µs)
    
    out of the:
    
      crd := &apiextensionsv1.CustomResourceDefinition{}
      err := c.client.Get(context.Background(), ctrlruntimeclient.ObjectKey{Name: "agenticruns.agentic.openshift.io"}, crd)
    
    call in pkg/agenticrun's Controller.crdAvailable, even when the CRD
    itself exists in the cluster:
    
      $ oc get customresourcedefinitions agenticruns.agentic.openshift.io
      NAME                               CREATED AT
      agenticruns.agentic.openshift.io   2026-07-24T00:40:30Z
    
    By registering scheme.AddToScheme (all the normal stuff) and
    apiextensionsv1.AddToScheme (the CRD type), we can successfully
    execute that Get.

--- pkg/start/start.go
+++ pkg/start/start.go
@@ -16,9 +16,11 @@ import (
 	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 
 	v1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/dynamic"
@@ -531,7 +533,13 @@ func (cb *ClientBuilder) DynamicClientOrDie(name string, configFns ...func(*rest
 }
 
 func (cb *ClientBuilder) RuntimeControllerClientOrDie(name string, configFns ...func(*rest.Config)) runtimeclient.Client {
-	c, err := runtimeclient.New(rest.AddUserAgent(cb.RestConfig(configFns...), name), runtimeclient.Options{})
+	runtimeScheme := runtime.NewScheme()
+	utilruntime.Must(scheme.AddToScheme(runtimeScheme))
+	utilruntime.Must(apiextensionsv1.AddToScheme(runtimeScheme))
+
+	c, err := runtimeclient.New(rest.AddUserAgent(cb.RestConfig(configFns...), name), runtimeclient.Options{
+		Scheme: runtimeScheme,
+	})
 	if err != nil {
 		panic(err)
 	}
